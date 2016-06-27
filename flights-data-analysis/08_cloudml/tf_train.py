d14ce0405fe4220cb5d324700ded7846f7ced3b2
vlakshmanan <vlakshmanan@google.com>
2016-06-27 12:51:27 -0700

    working trainer. batchsize makes no sense ...

--- flights-data-analysis/08_cloudml/tf_train.py
+++ flights-data-analysis/08_cloudml/tf_train.py
@@ -11,29 +11,29 @@ import os
 #bucket = client.get_bucket('cloud-training-demos')
 #infiles = bucket.list_blobs(prefix='/flights/chapter07/')
 
-infile = '/Users/vlakshmanan/data/flights/flights-00001-of-00012.csv'
-# read into pandas
-df = pd.read_csv(infile, header=None, names=['label','depdelay','taxiout','distance','avgdep','avgarr'])
 
-# scale inputs
-print df.describe()
-df['depdelay'] = df['depdelay']/60
-df['taxiout'] = df['taxiout']/30
-df['distance'] = df['distance']/3000
-df['avgdep'] = df['avgdep']/30
-df['avgarr'] = df['avgarr']/30
-print df.describe()
+# build the graph to read the training data
+def get_training_data():
+  # set up queue
+  infiles = ['/Users/vlakshmanan/data/flights/flights-00001-of-00012.csv', '/Users/vlakshmanan/data/flights/flights-00002-of-00012.csv']
+  filename_queue = tf.train.string_input_producer(infiles, num_epochs=None)
+  # read one example
+  reader = tf.TextLineReader(skip_header_lines=0)
+  _, line = reader.read(filename_queue)
+  record_defaults=[[1.0],[1.0],[1.0],[1.0],[1.0],[1.0]]
+  label, depdelay, taxiout, distance, avgdep, avgarr = tf.decode_csv(line, record_defaults=record_defaults)
+  features = tf.pack([depdelay,taxiout,distance,avgdep,avgarr])
+  labels   = tf.pack([label])
 
-predictors = df.iloc[:,1:6]
-targets = df.iloc[:,0]
+  # batch it
+  f, l = tf.train.batch([features, labels], batch_size=10000, num_threads=3)
+  return f, l
 
-trainsize = len(targets)
-npredictors = len(predictors.columns)
-noutputs = 1
-nhidden = 5
-numiter = 10000
-modelfile = '/tmp/trained_model'
-with tf.Session() as sess:
+# build the neural network graph
+def get_nn():
+  npredictors = 5
+  nhidden = 7
+  noutputs = 1
   feature_data = tf.placeholder("float", [None, npredictors])
   target_data = tf.placeholder("float", [None, noutputs])
   weights1 = tf.Variable(tf.truncated_normal([npredictors, nhidden], stddev=0.01))
@@ -41,24 +41,42 @@ with tf.Session() as sess:
   biases1 = tf.Variable(tf.ones([nhidden]))
   biases2 = tf.Variable(tf.ones([noutputs]))
   model = tf.sigmoid(tf.matmul(tf.nn.relu(tf.matmul(feature_data, weights1) + biases1), weights2) + biases2)
-  cost = tf.nn.l2_loss(tf.nn.sigmoid_cross_entropy_with_logits(model, target_data))
-  training_step = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
-  init = tf.initialize_all_variables()
-  sess.run(init)
-
   saver = tf.train.Saver({'weights1' : weights1, 'biases1' : biases1, 'weights2' : weights2, 'biases2' : biases2})
-  for iter in xrange(0, numiter):
-    sess.run(training_step, feed_dict = {
-        feature_data : predictors[:trainsize].values,
-        target_data : targets[:trainsize].values.reshape(trainsize, noutputs)
-      })
-    
-    if iter%1000 == 0:
-      print '{0} error={1}'.format(iter, np.sqrt(cost.eval(feed_dict = {
-          feature_data : predictors[:trainsize].values,
-          target_data : targets[:trainsize].values.reshape(trainsize, noutputs)
-      }) / trainsize))
-    
-  filename = saver.save(sess, modelfile, global_step=numiter)
+  return model, saver, feature_data, target_data
+
+############ 'main' starts here ##############
+numbatches = 300
+modelfile = '/tmp/trained_model'
+with tf.Session() as sess:
+  # create the computation graph
+  features, labels = get_training_data()
+  model, saver, feature_data, target_data = get_nn()
+  cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(model, target_data))
+  optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
+  training_step = optimizer.minimize(cost)
+ 
+  tf.initialize_all_variables().run()
+
+  tf.get_default_graph().finalize()  #prevent changing graph
+
+  # start the training
+  coord = tf.train.Coordinator()
+  threads = tf.train.start_queue_runners(coord=coord)
+
+  try: 
+    nbatch = 0
+    while nbatch < numbatches:
+       features_feed, labels_feed = sess.run([features, labels])
+       result = sess.run(training_step, feed_dict = {feature_data: features_feed, target_data: labels_feed})
+       nbatch = nbatch + 1
+       if nbatch%10 == 0:
+           # Q: does this make training_step skip this data?
+           print "batchno={0} cost={1}".format(nbatch, sess.run(cost, feed_dict = {feature_data: features_feed, target_data: labels_feed}))
+  except tf.errors.OutOfRangeError as e:
+     print "Ran out of inputs (?!)"
+  finally:
+     coord.request_stop()
+  coord.join(threads)
+  filename = saver.save(sess, modelfile, global_step=numbatches)
   print 'Model written to {0}'.format(filename)
 
