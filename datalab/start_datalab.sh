f99799a59406ddf15b94ea823ab5ac14a471d510
lakshmanok <lakshmanok@gmail.com>
2016-08-15 18:38:59 -0700

    docker needs sudo

--- datalab/start_datalab.sh
+++ datalab/start_datalab.sh
@@ -2,7 +2,7 @@
 
 VMIP=`wget -qO - http://ipecho.net/plain`
 echo "Browse to http://$VMIP:8081/"
-docker run -t \
+sudo docker run -t \
    -p "8081:8080" \
    -v "${HOME}:/content" \
    gcr.io/cloud-datalab/datalab:local
