995b5f5e6ba01ee4a69cc9e595e5d5f7f8e236fc
lakshmanok <lakshmanok@gmail.com>
2016-10-17 22:07:47 +0000

    debug

---
+++ CPB101/lab2/python/grepc.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""
+Copyright Google Inc. 2016
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import apache_beam as beam
+
+def my_grep(line, term):
+   if term in line:
+      yield line
+
+PROJECT='cloud-training-demos'
+BUCKET='cloud-training-demos'
+
+if __name__ == '__main__':
+   argv = [
+      '--project={0}'.format(PROJECT),
+      '--job_name=examplejob',
+      '--staging_location=gs://{0}/staging/'.format(BUCKET),
+      '--save_main_session',
+      '--temp_location=gs://{0}/staging/'.format(BUCKET),
+      '--runner=DataflowPipelineRunner'
+   ]
+
+   p = beam.Pipeline(argv=argv)
+   input = 'gs://cloud-training-demos/javahelp/*.java'
+   output_prefix = 'gs://cloud-training-demos/javahelp/output'
+   searchTerm = 'import'
+
+   # find all lines that contain the searchTerm
+   (p
+      | 'GetJava' >> beam.Read(beam.io.TextFileSource(input))
+      | 'Grep' >> beam.FlatMap(lambda line: my_grep(line, searchTerm) )
+      | 'write' >> beam.io.textio.WriteToText(output_prefix)
+   )
+
+   p.run()
+
