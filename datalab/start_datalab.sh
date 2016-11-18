4f210eee3e30808914bddd9e79afa6e40436a3e2
Lak Lakshmanan <vlakshmanan@google.com>
2016-11-18 08:52:53 -0800

    how to set project id

--- datalab/start_datalab.sh
+++ datalab/start_datalab.sh
@@ -2,6 +2,12 @@
 
 IMAGE=gcr.io/cloud-datalab/datalab:local   # release
 
+# optionally, add a line like this to the docker run command:
+# (change your project-id to match)
+# if you don't do this, datalab will ask you to sign in and
+# set a project-id
+#   -e "PROJECT_ID=cloud-training-demos" \
+
 docker pull $IMAGE
 docker run -t \
    -p "8081:8080" \
