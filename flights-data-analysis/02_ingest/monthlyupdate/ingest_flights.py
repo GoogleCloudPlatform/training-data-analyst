fad836e50a82e6bcbee70d530833f9444ce29253
Lak Lakshmanan <vlakshmanan@google.com>
2016-11-25 18:37:18 -0800

    use logging api instead of print statements.

--- flights-data-analysis/02_ingest/monthlyupdate/ingest_flights.py
+++ flights-data-analysis/02_ingest/monthlyupdate/ingest_flights.py
@@ -16,10 +16,11 @@
 
 import os
 import shutil
+import logging
+import os.path
 import zipfile
 import datetime
 import tempfile
-import os.path
 from urllib2 import urlopen
 from google.cloud import storage
 from google.cloud.storage import Blob
@@ -30,7 +31,7 @@ def download(YEAR, MONTH, destdir):
      YEAR e.g.'2015'
      MONTH e.g. '01 for January
    '''
-   print ('Requesting data for {}-{}-*'.format(YEAR, MONTH))
+   logging.info('Requesting data for {}-{}-*'.format(YEAR, MONTH))
 
    PARAMS="UserTableName=On_Time_Performance&DBShortName=&RawDataTable=T_ONTIME&sqlstr=+SELECT+FL_DATE%2CUNIQUE_CARRIER%2CAIRLINE_ID%2CCARRIER%2CFL_NUM%2CORIGIN_AIRPORT_ID%2CORIGIN_AIRPORT_SEQ_ID%2CORIGIN_CITY_MARKET_ID%2CORIGIN%2CDEST_AIRPORT_ID%2CDEST_AIRPORT_SEQ_ID%2CDEST_CITY_MARKET_ID%2CDEST%2CCRS_DEP_TIME%2CDEP_TIME%2CDEP_DELAY%2CTAXI_OUT%2CWHEELS_OFF%2CWHEELS_ON%2CTAXI_IN%2CCRS_ARR_TIME%2CARR_TIME%2CARR_DELAY%2CCANCELLED%2CCANCELLATION_CODE%2CDIVERTED%2CDISTANCE+FROM++T_ONTIME+WHERE+Month+%3D{1}+AND+YEAR%3D{0}&varlist=FL_DATE%2CUNIQUE_CARRIER%2CAIRLINE_ID%2CCARRIER%2CFL_NUM%2CORIGIN_AIRPORT_ID%2CORIGIN_AIRPORT_SEQ_ID%2CORIGIN_CITY_MARKET_ID%2CORIGIN%2CDEST_AIRPORT_ID%2CDEST_AIRPORT_SEQ_ID%2CDEST_CITY_MARKET_ID%2CDEST%2CCRS_DEP_TIME%2CDEP_TIME%2CDEP_DELAY%2CTAXI_OUT%2CWHEELS_OFF%2CWHEELS_ON%2CTAXI_IN%2CCRS_ARR_TIME%2CARR_TIME%2CARR_DELAY%2CCANCELLED%2CCANCELLATION_CODE%2CDIVERTED%2CDISTANCE&grouplist=&suml=&sumRegion=&filter1=title%3D&filter2=title%3D&geo=All%A0&time=March&timename=Month&GEOGRAPHY=All&XYEAR={0}&FREQUENCY=3&VarDesc=Year&VarType=Num&VarDesc=Quarter&VarType=Num&VarDesc=Month&VarType=Num&VarDesc=DayofMonth&VarType=Num&VarDesc=DayOfWeek&VarType=Num&VarName=FL_DATE&VarDesc=FlightDate&VarType=Char&VarName=UNIQUE_CARRIER&VarDesc=UniqueCarrier&VarType=Char&VarName=AIRLINE_ID&VarDesc=AirlineID&VarType=Num&VarName=CARRIER&VarDesc=Carrier&VarType=Char&VarDesc=TailNum&VarType=Char&VarName=FL_NUM&VarDesc=FlightNum&VarType=Char&VarName=ORIGIN_AIRPORT_ID&VarDesc=OriginAirportID&VarType=Num&VarName=ORIGIN_AIRPORT_SEQ_ID&VarDesc=OriginAirportSeqID&VarType=Num&VarName=ORIGIN_CITY_MARKET_ID&VarDesc=OriginCityMarketID&VarType=Num&VarName=ORIGIN&VarDesc=Origin&VarType=Char&VarDesc=OriginCityName&VarType=Char&VarDesc=OriginState&VarType=Char&VarDesc=OriginStateFips&VarType=Char&VarDesc=OriginStateName&VarType=Char&VarDesc=OriginWac&VarType=Num&VarName=DEST_AIRPORT_ID&VarDesc=DestAirportID&VarType=Num&VarName=DEST_AIRPORT_SEQ_ID&VarDesc=DestAirportSeqID&VarType=Num&VarName=DEST_CITY_MARKET_ID&VarDesc=DestCityMarketID&VarType=Num&VarName=DEST&VarDesc=Dest&VarType=Char&VarDesc=DestCityName&VarType=Char&VarDesc=DestState&VarType=Char&VarDesc=DestStateFips&VarType=Char&VarDesc=DestStateName&VarType=Char&VarDesc=DestWac&VarType=Num&VarName=CRS_DEP_TIME&VarDesc=CRSDepTime&VarType=Char&VarName=DEP_TIME&VarDesc=DepTime&VarType=Char&VarName=DEP_DELAY&VarDesc=DepDelay&VarType=Num&VarDesc=DepDelayMinutes&VarType=Num&VarDesc=DepDel15&VarType=Num&VarDesc=DepartureDelayGroups&VarType=Num&VarDesc=DepTimeBlk&VarType=Char&VarName=TAXI_OUT&VarDesc=TaxiOut&VarType=Num&VarName=WHEELS_OFF&VarDesc=WheelsOff&VarType=Char&VarName=WHEELS_ON&VarDesc=WheelsOn&VarType=Char&VarName=TAXI_IN&VarDesc=TaxiIn&VarType=Num&VarName=CRS_ARR_TIME&VarDesc=CRSArrTime&VarType=Char&VarName=ARR_TIME&VarDesc=ArrTime&VarType=Char&VarName=ARR_DELAY&VarDesc=ArrDelay&VarType=Num&VarDesc=ArrDelayMinutes&VarType=Num&VarDesc=ArrDel15&VarType=Num&VarDesc=ArrivalDelayGroups&VarType=Num&VarDesc=ArrTimeBlk&VarType=Char&VarName=CANCELLED&VarDesc=Cancelled&VarType=Num&VarName=CANCELLATION_CODE&VarDesc=CancellationCode&VarType=Char&VarName=DIVERTED&VarDesc=Diverted&VarType=Num&VarDesc=CRSElapsedTime&VarType=Num&VarDesc=ActualElapsedTime&VarType=Num&VarDesc=AirTime&VarType=Num&VarDesc=Flights&VarType=Num&VarName=DISTANCE&VarDesc=Distance&VarType=Num&VarDesc=DistanceGroup&VarType=Num&VarDesc=CarrierDelay&VarType=Num&VarDesc=WeatherDelay&VarType=Num&VarDesc=NASDelay&VarType=Num&VarDesc=SecurityDelay&VarType=Num&VarDesc=LateAircraftDelay&VarType=Num&VarDesc=FirstDepTime&VarType=Char&VarDesc=TotalAddGTime&VarType=Num&VarDesc=LongestAddGTime&VarType=Num&VarDesc=DivAirportLandings&VarType=Num&VarDesc=DivReachedDest&VarType=Num&VarDesc=DivActualElapsedTime&VarType=Num&VarDesc=DivArrDelay&VarType=Num&VarDesc=DivDistance&VarType=Num&VarDesc=Div1Airport&VarType=Char&VarDesc=Div1AirportID&VarType=Num&VarDesc=Div1AirportSeqID&VarType=Num&VarDesc=Div1WheelsOn&VarType=Char&VarDesc=Div1TotalGTime&VarType=Num&VarDesc=Div1LongestGTime&VarType=Num&VarDesc=Div1WheelsOff&VarType=Char&VarDesc=Div1TailNum&VarType=Char&VarDesc=Div2Airport&VarType=Char&VarDesc=Div2AirportID&VarType=Num&VarDesc=Div2AirportSeqID&VarType=Num&VarDesc=Div2WheelsOn&VarType=Char&VarDesc=Div2TotalGTime&VarType=Num&VarDesc=Div2LongestGTime&VarType=Num&VarDesc=Div2WheelsOff&VarType=Char&VarDesc=Div2TailNum&VarType=Char&VarDesc=Div3Airport&VarType=Char&VarDesc=Div3AirportID&VarType=Num&VarDesc=Div3AirportSeqID&VarType=Num&VarDesc=Div3WheelsOn&VarType=Char&VarDesc=Div3TotalGTime&VarType=Num&VarDesc=Div3LongestGTime&VarType=Num&VarDesc=Div3WheelsOff&VarType=Char&VarDesc=Div3TailNum&VarType=Char&VarDesc=Div4Airport&VarType=Char&VarDesc=Div4AirportID&VarType=Num&VarDesc=Div4AirportSeqID&VarType=Num&VarDesc=Div4WheelsOn&VarType=Char&VarDesc=Div4TotalGTime&VarType=Num&VarDesc=Div4LongestGTime&VarType=Num&VarDesc=Div4WheelsOff&VarType=Char&VarDesc=Div4TailNum&VarType=Char&VarDesc=Div5Airport&VarType=Char&VarDesc=Div5AirportID&VarType=Num&VarDesc=Div5AirportSeqID&VarType=Num&VarDesc=Div5WheelsOn&VarType=Char&VarDesc=Div5TotalGTime&VarType=Num&VarDesc=Div5LongestGTime&VarType=Num&VarDesc=Div5WheelsOff&VarType=Char&VarDesc=Div5TailNum&VarType=Char".format(YEAR, MONTH)
 
@@ -39,7 +40,7 @@ def download(YEAR, MONTH, destdir):
    with open(filename, "wb") as fp:
      response = urlopen(url, PARAMS)
      fp.write(response.read())
-   print ("{} saved".format(filename))
+   logging.debug("{} saved".format(filename))
    return filename
 
 def zip_to_csv(filename, destdir):
@@ -50,7 +51,7 @@ def zip_to_csv(filename, destdir):
    os.chdir(cwd)
    csvfile = os.path.join(destdir, zip_ref.namelist()[0])
    zip_ref.close()
-   print ("Extracted {}".format(csvfile))
+   logging.info("Extracted {}".format(csvfile))
    return csvfile
 
 
@@ -74,13 +75,13 @@ def remove_quotes_comma(csvfile, year, month):
            outfp.write('\n')
            N = N + 1
    if N > 1:
-      print ('Cleaned up {} lines and wrote {} ...'.format(N, outfile))
+      logging.debug('Cleaned up {} lines and wrote {} ...'.format(N, outfile))
       return outfile
    else:
       os.remove(outfile)
       raise DataUnavailable('Empty file from BTS')
  finally:
-   print ("... removing {}".format(csvfile))
+   logging.debug("... removing {}".format(csvfile))
    os.remove(csvfile)
 
 def upload(csvfile, bucketname, blobname):
@@ -89,7 +90,7 @@ def upload(csvfile, bucketname, blobname):
    blob = Blob(blobname, bucket)
    blob.upload_from_filename(csvfile)
    gcslocation = 'gs://{}/{}'.format(bucketname, blobname)
-   print ('Uploaded {} ...'.format(gcslocation))
+   logging.info('Uploaded {} ...'.format(gcslocation))
    return gcslocation
 
 def ingest(year, month, bucket):
@@ -106,7 +107,7 @@ def ingest(year, month, bucket):
       gcsloc = 'flights/{}'.format(os.path.basename(csvfile))
       return upload(csvfile, bucket, gcsloc)
    finally:
-      print ('Cleaning up by removing {}'.format(tempdir))
+      logging.debug('Cleaning up by removing {}'.format(tempdir))
       shutil.rmtree(tempdir)
 
 def next_month(bucketname):
@@ -118,12 +119,12 @@ def next_month(bucketname):
    blobs  = list(bucket.list_blobs(prefix='flights/'))
    files = [blob.name for blob in blobs if 'csv' in blob.name] # csv files only
    lastfile = os.path.basename(files[-1])
-   print ('The latest file on GCS is {}'.format(lastfile))
+   logging.debug('The latest file on GCS is {}'.format(lastfile))
    year = lastfile[:4]
    month = lastfile[4:6]
    dt = datetime.datetime(int(year), int(month), 15) # 15th of month
    dt = dt + datetime.timedelta(30) # will always go to next month
-   print ('The next month is {}'.format(dt))
+   logging.debug('The next month is {}'.format(dt))
    return '{}'.format(dt.year), '{:02d}'.format(dt.month)
   
 if __name__ == '__main__':
@@ -134,15 +135,16 @@ if __name__ == '__main__':
    parser.add_argument('--month', help='Specify 01 for January. If not provided, defaults to getting next month')
 
    try:
+      logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
       args = parser.parse_args()
       if args.year is None or args.month is None:
          year, month = next_month(args.bucket)
       else:
          year = args.year
          month = args.month
-      print ('Ingesting year={} month={}'.format(year, month))
+      logging.debug('Ingesting year={} month={}'.format(year, month))
       gcsfile = ingest(year, month, args.bucket)
-      print ('Success ... ingested to {}'.format(gcsfile))
+      logging.info('Success ... ingested to {}'.format(gcsfile))
    except DataUnavailable as e:
-      print ('Try again later: {}'.format(e.message))
+      logging.info('Try again later: {}'.format(e.message))
 
