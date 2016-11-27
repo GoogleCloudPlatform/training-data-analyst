1ae52e2e176d6a8a78d96c308bb5536705fc3994
Anthonios Partheniou <apartheniou@electricalengineer.ca>
2016-11-27 09:50:22 -0500

    Update the start_datalab.sh to add ability to set the notebook directory

--- datalab/start_datalab.sh
+++ datalab/start_datalab.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
+# If the PROJECT_ID environment variable is not set, Datalab will ask you
+# to set a project id when you try to use GCP resources such as BigQuery.
+# PROJECT_ID='your-project-id'
 IMAGE=gcr.io/cloud-datalab/datalab:local   # release
 
-# On linux, docker runs directly on host machine. It's important to bind
-# to 127.0.0.1 only to prevent Datalab from accessible on the local network.
-# On other platforms, bind to all ip addresses so VirtualBox can
-# access it. Users need to make sure that their VirtualBox port forwarding
-# setting only binds to 127.0.0.1.
+# By default, the home directory is used to store Cloud Datalab notebooks and
+# configurations. If you want to change this directory, simply update the
+# CONTENT variable below to the desired directory.
+CONTENT=${HOME}
+
+if [ ! -d $CONTENT ]; then
+    echo "CONTENT directory does not exist. Please check start_datalab.sh"
+    exit 1
+fi
+
+cd $CONTENT
+if [ ! -d "training-data-analyst" ]; then
+   git clone http://github.com/GoogleCloudPlatform/training-data-analyst/
+fi
+
+# On linux, docker runs directly on the host machine. It's important to bind to
+# 127.0.0.1 only to ensure that Datalab is not accessible on your local network.
+# On other platforms, bind to all ip addresses so VirtualBox can access it.
+# Users need to make sure that their VirtualBox port forwarding setting only
+# binds to 127.0.0.1.
 if [ "$OSTYPE" == "linux"* ]; then
   PORTMAP="127.0.0.1:8081:8080"
 else
   PORTMAP="8081:8080"
 fi
 
-# optionally, add a line like this to the docker run command:
-# (change your project-id to match)
-# if you don't do this, datalab will ask you to sign in and
-# set a project-id
-#   -e "PROJECT_ID=cloud-training-demos" \
-
 docker pull $IMAGE
-docker run -t \
+docker run -it \
    -p $PORTMAP \
-   -v "${HOME}:/content" \
-   $IMAGE &
+   -v "$CONTENT:/content" \
+   -e "PROJECT_ID=$PROJECT_ID" \
+   $IMAGE
