6793f37b10cbf162f83db5e80b0b073c480388b5
Lak Lakshmanan <vlakshmanan@google.com>
2016-12-08 20:48:17 -0800

    publishing to pub/sub

--- flights-data-analysis/03_sqlstudio/simulate/simulate.py
+++ flights-data-analysis/03_sqlstudio/simulate/simulate.py
@@ -14,29 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import google.cloud.bigquery as bq
-import argparse
-import logging
-import datetime
 import time
 import pytz
+import logging
+import argparse
+import datetime
+from google.cloud import pubsub
+import google.cloud.bigquery as bq
 
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S %Z'
 
-def notify(rows, simStartTime, programStart, speedFactor):
+def publish(topics, allevents):
+   for key in topics:  # 'departed', 'arrived', etc.
+      topic = topics[key]
+      events = allevents[key]
+      with topic.batch() as batch:
+         logging.info('Publishing {} {} events'.format(len(events), key))
+         for event_data in events:
+              batch.publish(event_data)
+
+def notify(topics, rows, simStartTime, programStart, speedFactor):
+   # sleep computation
+   def compute_sleep_secs(notify_time):
+        time_elapsed = (datetime.datetime.utcnow() - programStart).seconds
+        sim_time_elapsed = (notify_time - simStartTime).seconds / speedFactor
+        to_sleep_secs = sim_time_elapsed - time_elapsed
+        return to_sleep_secs
+
+   tonotify = {}
+   for key in topics:
+     tonotify[key] = list()
+
    for row in rows:
        event, notify_time, event_data = row
+       tonotify[event].append(event_data)
 
        # how much time should we sleep?
-       time_elapsed = (datetime.datetime.utcnow() - programStart).seconds
-       sim_time_elapsed = (notify_time - simStartTime).seconds / speedFactor
-       to_sleep_secs = sim_time_elapsed - time_elapsed
-       if to_sleep_secs > 0:
-          logging.info('Sleeping {} seconds'.format(to_sleep_secs))
-          time.sleep(to_sleep_secs)
+       if compute_sleep_secs(notify_time) > 0:
+          # notify the accumulated tonotify
+          publish(topics, tonotify)
+          for key in topics:
+             tonotify[key] = list()
 
-       # notify to Pub/Sub
-       print event_data.split(',')[2:8]
+          # recompute sleep, since notification takes a while
+          to_sleep_secs = compute_sleep_secs(notify_time)
+          if to_sleep_secs > 0:
+             logging.info('Sleeping {} seconds'.format(to_sleep_secs))
+             time.sleep(to_sleep_secs)
+
+   # left-over records; notify again
+   publish(topics, tonotify)
 
 
 if __name__ == '__main__':
@@ -45,16 +72,16 @@ if __name__ == '__main__':
    parser.add_argument('--endTime', help='Example: 2015-05-03 00:00:00 UTC', required=True)
    parser.add_argument('--speedFactor', help='Example: 60 implies 1 hour of data sent to Cloud Pub/Sub in 1 minute', required=True, type=float)
 
-   # set up BigQuery client
+   # set up BigQuery bqclient
    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
    args = parser.parse_args()
-   client = bq.Client()
-   dataset = client.dataset('flights')
+   bqclient = bq.Client()
+   dataset = bqclient.dataset('flights')
    if not dataset.exists():
       logging.error('Did not find a dataset named <flights> in your project')
       exit(-1)
   
-   # set up the query to pull simulated events
+   # run the query to pull simulated events
    querystr = """\
 SELECT
   EVENT,
@@ -68,13 +95,14 @@ WHERE
 ORDER BY
   NOTIFY_TIME ASC
 """
-   query = client.run_sync_query(querystr.format(args.startTime,
+   query = bqclient.run_sync_query(querystr.format(args.startTime,
                                                  args.endTime))
    query.use_legacy_sql = False # standard SQL
    query.timeout_ms = 2000
    query.max_results = 100  # at a time
    query.run()
 
+   # wait for query to complete and fetch first page of data
    if query.complete:
       rows = query.rows
       token = query.page_token
@@ -94,11 +122,20 @@ ORDER BY
          exit(-1)
       rows, total_count, token = query.fetch_data()
 
+   # create one Pub/Sub notification topic for each type of event
+   psclient = pubsub.Client()
+   topics = {}
+   for event_type in ['departed', 'arrived']:
+       topics[event_type] = psclient.topic(event_type)
+       if not topics[event_type].exists():
+          topics[event_type].create()
+   
+   # notify about each row in the dataset
    programStartTime = datetime.datetime.utcnow() 
    simStartTime = datetime.datetime.strptime(args.startTime, TIME_FORMAT).replace(tzinfo=pytz.UTC)
    print 'Simulation start time is {}'.format(simStartTime)
    while True:
-      notify(rows, simStartTime, programStartTime, args.speedFactor)
+      notify(topics, rows, simStartTime, programStartTime, args.speedFactor)
       if token is None:
          break
       rows, total_count, token = query.fetch_data(page_token=token)
