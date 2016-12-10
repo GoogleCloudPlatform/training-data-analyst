b88e7c3d996f1f968f3ba81212538915172edaf1
Lak Lakshmanan <vlakshmanan@google.com>
2016-12-10 15:49:51 -0800

    Working aggregate statistics Dataflow pipeline

--- flights-data-analysis/03_sqlstudio/realtime/chapter3/src/main/java/com/google/cloud/training/flights/AverageDelayPipeline.java
+++ flights-data-analysis/03_sqlstudio/realtime/chapter3/src/main/java/com/google/cloud/training/flights/AverageDelayPipeline.java
@@ -25,8 +25,6 @@ import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
 import com.google.cloud.dataflow.sdk.Pipeline;
-import com.google.cloud.dataflow.sdk.coders.AvroCoder;
-import com.google.cloud.dataflow.sdk.coders.DefaultCoder;
 import com.google.cloud.dataflow.sdk.io.BigQueryIO;
 import com.google.cloud.dataflow.sdk.io.PubsubIO;
 import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions;
@@ -37,6 +35,7 @@ import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.Max;
 import com.google.cloud.dataflow.sdk.transforms.Mean;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.transforms.Sum;
 import com.google.cloud.dataflow.sdk.transforms.join.CoGbkResult;
 import com.google.cloud.dataflow.sdk.transforms.join.CoGroupByKey;
 import com.google.cloud.dataflow.sdk.transforms.join.KeyedPCollectionTuple;
@@ -71,75 +70,79 @@ public class AverageDelayPipeline {
 	}
 	
 	public static void main(String[] args) {
-		MyOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(MyOptions.class);
+		MyOptions options = PipelineOptionsFactory.fromArgs(args).//
+				withValidation().as(MyOptions.class);
 		options.setStreaming(true);
 		Pipeline p = Pipeline.create(options);
 
 		// output
 		String outputTable = options.getProject() + ':' + "flights.streaming_delays";
-		TableSchema schema = createSchema(
-				"airport:string,latitude:float,longitude:float,timestamp:timestamp,dep_delay:float,arr_delay:float");
+		TableSchema schema = createSchema("airport:string,latitude:float,longitude:float,"
+				+ "timestamp:timestamp,dep_delay:float,arr_delay:float,num_flights:integer");
 
 		// compute moving averages
 		final WindowStats arr = movingAverageOf(options, p, "arrived");
-		//final WindowStats dep = movingAverageOf(options, p, "departed");
+		final WindowStats dep = movingAverageOf(options, p, "departed");
 		
-		// make them local variables, otherwise Java tries to serialize them
+		// make them local variables, otherwise Java tries to serialize them into the anonymous class
 		final PCollection<KV<Airport, Double>> arr_delay = arr.delay;
 		final PCollection<KV<Airport, String>> arr_timestamp = arr.timestamp;
-		//final PCollection<KV<Airport, Double>> dep_delay = dep.delay;
-		//final PCollection<KV<Airport, String>> dep_timestamp = dep.timestamp;
+		final PCollection<KV<Airport, Integer>> arr_num_flights = arr.num_flights;
+		final PCollection<KV<Airport, Double>> dep_delay = dep.delay;
+		final PCollection<KV<Airport, String>> dep_timestamp = dep.timestamp;
+		final PCollection<KV<Airport, Integer>> dep_num_flights = dep.num_flights;
 		final TupleTag<Double> arr_delayTag = new TupleTag<Double>();
 		final TupleTag<String> arr_tsTag = new TupleTag<String>();
-		//final TupleTag<Double> dep_delayTag = new TupleTag<Double>();
-		//final TupleTag<String> dep_tsTag = new TupleTag<String>();
+		final TupleTag<Integer> arr_nfTag = new TupleTag<Integer>();
+		final TupleTag<Double> dep_delayTag = new TupleTag<Double>();
+		final TupleTag<String> dep_tsTag = new TupleTag<String>();
+		final TupleTag<Integer> dep_nfTag = new TupleTag<Integer>();
 		
-//		// join
-//		KeyedPCollectionTuple //
-//				.of(arr_delayTag, arr_delay) //
-//				//.and(dep_delayTag, dep_delay) //
-//				.and(arr_tsTag, arr_timestamp) //
-//				//.and(dep_tsTag, dep_timestamp) //
-//				.apply("cogroup", CoGroupByKey.<Airport> create()) //
-//				.apply("join_delays", ParDo.of(new DoFn<KV<Airport, CoGbkResult>, Airport>() {
-//
-//					@Override
-//					public void processElement(ProcessContext c) throws Exception {
-//						KV<Airport, CoGbkResult> e = c.element();
-//						Airport airport = e.getKey();
-//						Double arrDelay = e.getValue().getOnly(arr_delayTag, new Double(-999));
-//						//Double depDelay = e.getValue().getOnly(dep_delayTag, new Double(-999));
-//						String arrTs = e.getValue().getOnly(arr_tsTag, "");
-//						//String depTs = e.getValue().getOnly(dep_tsTag, "");
-//						//String timestamp = (arrTs.compareTo(depTs) > 0)? arrTs : depTs; // latest
-//						//c.output(new Airport(airport, arrDelay, depDelay, timestamp));
-//						c.output(new Airport(airport, arrDelay, arrDelay, arrTs));
-//					}
-//
-//				}))//
-		
-		arr_delay.apply(ParDo.of(new DoFn<KV<Airport, Double>, Airport>() {
-			public void processElement(ProcessContext c) throws Exception {
-				Airport airport = c.element().getKey();
-				Double arrDelay = c.element().getValue();
-				c.output(new Airport(airport, arrDelay, arrDelay, "2015-05-01 00:00:00 UTC"));
-			}
-		}))//
-				.apply("toBQRow", ParDo.of(new DoFn<Airport, TableRow>() {
+		// join
+		KeyedPCollectionTuple //
+				.of(arr_delayTag, arr_delay) //
+				.and(dep_delayTag, dep_delay) //
+				.and(arr_tsTag, arr_timestamp) //
+				.and(dep_tsTag, dep_timestamp) //
+				.and(arr_nfTag, arr_num_flights) //
+				.and(dep_nfTag, dep_num_flights) //
+				.apply("airport:cogroup", CoGroupByKey.<Airport> create()) //
+				.apply("airport:stats", ParDo.of(new DoFn<KV<Airport, CoGbkResult>, AirportStats>() {
+
 					@Override
 					public void processElement(ProcessContext c) throws Exception {
-						Airport airport = c.element();
+						KV<Airport, CoGbkResult> e = c.element();
+						Airport airport = e.getKey();
+						Double arrDelay = e.getValue().getOnly(arr_delayTag, new Double(-999));
+						Double depDelay = e.getValue().getOnly(dep_delayTag, new Double(-999));
+						String arrTs = e.getValue().getOnly(arr_tsTag, "");
+						String depTs = e.getValue().getOnly(dep_tsTag, "");
+						String timestamp = (arrTs.compareTo(depTs) > 0)? arrTs : depTs; // latest
+						int num_flights = //
+								e.getValue().getOnly(arr_nfTag, new Integer(0)).intValue() + //
+								e.getValue().getOnly(dep_nfTag, new Integer(0)).intValue();
+						c.output(new AirportStats(airport, arrDelay, depDelay, timestamp, num_flights));
+					}
+
+				}))//
+				.apply("airport:to_BQrow", ParDo.of(new DoFn<AirportStats, TableRow>() {
+					@Override
+					public void processElement(ProcessContext c) throws Exception {
+						AirportStats stats = c.element();
 						TableRow row = new TableRow();
-						row.set("timestamp", airport.timestamp);
-						row.set("airport", airport.airport);
-						row.set("latitude", airport.latitude);
-						row.set("longitude", airport.longitude);
-						row.set("dep_delay", airport.dep_delay);
-						row.set("arr_delay", airport.arr_delay);
+						row.set("timestamp", stats.timestamp);
+						row.set("airport", stats.airport.name);
+						row.set("latitude", stats.airport.latitude);
+						row.set("longitude", stats.airport.longitude);
+						if (stats.dep_delay > -998)
+							row.set("dep_delay", stats.dep_delay); // else null
+						if (stats.arr_delay > -998)
+							row.set("arr_delay", stats.arr_delay); // else null
+						row.set("num_flights", stats.num_flights);
 						c.output(row);
 					}
 				}))//
-				.apply("write_to_bq",
+				.apply("airport:write_toBQ",
 						BigQueryIO.Write.to(outputTable) //
 								.withSchema(schema)//
 								.withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
@@ -147,85 +150,10 @@ public class AverageDelayPipeline {
 		p.run();
 	}
 
-	@DefaultCoder(AvroCoder.class)
-	public static class Airport implements Comparable<Airport> {
-		String airport; // the only field used in comparisons
-		double latitude;
-		double longitude;
-		double arr_delay;
-		double dep_delay;
-		String timestamp;
-
-		public Airport(String line, String event) {
-			int nameFieldNumber, latFieldNumber, lonFieldNumber, delayFieldNumber, timeFieldNumber;
-			if (event.equals("arrived")) {
-				nameFieldNumber = 13; // DEST
-				latFieldNumber = 31; // ARR_AIRPORT_LAT
-				delayFieldNumber = 23; // ARR_DELAY
-				timeFieldNumber = 22; // ARR_TIME
-			} else {
-				nameFieldNumber = 9; // ORIGIN
-				latFieldNumber = 28; // DEP_AIRPORT_LAT
-				delayFieldNumber = 16; // DEP_DELAY
-				timeFieldNumber = 15; // DEP_TIME
-			}
-			lonFieldNumber = latFieldNumber + 1;
-
-			String[] fields = line.split(",");	
-			this.airport = fields[nameFieldNumber - 1];
-			this.latitude = Double.parseDouble(fields[latFieldNumber - 1]);
-			this.longitude = Double.parseDouble(fields[lonFieldNumber - 1]);
-			this.arr_delay = this.dep_delay = Double.parseDouble(fields[delayFieldNumber - 1]);
-			this.timestamp = fields[timeFieldNumber - 1];
-		}
-
-		public Airport(Airport other, Double arrDelay, Double depDelay, String timestamp) {
-			this.airport = other.airport;
-			this.latitude = other.latitude;
-			this.longitude = other.longitude;
-			this.arr_delay = arrDelay;
-			this.dep_delay = depDelay;
-			this.timestamp = timestamp;
-		}
-
-		public Airport() {
-			// for Avro only
-		}
-		
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((airport == null) ? 0 : airport.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Airport other = (Airport) obj;
-			if (airport == null) {
-				if (other.airport != null)
-					return false;
-			} else if (!airport.equals(other.airport))
-				return false;
-			return true;
-		}
-
-		@Override
-		public int compareTo(Airport other) {
-			return this.airport.compareTo(other.airport);
-		}
-	}
-
 	public static class WindowStats {
 		PCollection<KV<Airport, Double>> delay;
 		PCollection<KV<Airport, String>> timestamp;
+		PCollection<KV<Airport, Integer>> num_flights;
 	}
 
 	private static WindowStats movingAverageOf(MyOptions options, Pipeline p, String event) {
@@ -239,18 +167,20 @@ public class AverageDelayPipeline {
 		System.out.println("Averaging freq = " + averagingFrequency);
 		
 		String topic = "projects/" + options.getProject() + "/topics/" + event;
-		PCollection<Airport> airports = p //
+		final FieldNumberLookup eventType = FieldNumberLookup.create(event);
+		PCollection<Flight> flights = p //
 				.apply(event + ":read", PubsubIO.Read.topic(topic)) //
 				.apply(event + ":window",
 						Window.into(SlidingWindows//
 								.of(averagingInterval)//
 								.every(averagingFrequency))) //
-				.apply(event + ":parse", ParDo.of(new DoFn<String, Airport>() {
+				.apply(event + ":parse", ParDo.of(new DoFn<String, Flight>() {
 					@Override
 					public void processElement(ProcessContext c) throws Exception {
 						try {
-							Airport airport = new Airport(c.element(), event);
-							c.output(airport);
+							String line = c.element();
+							Flight f = new Flight(line.split(","), eventType);
+							c.output(f);
 						} catch (NumberFormatException e) {
 							// ignore errors about empty delay fields ...
 						}
@@ -258,25 +188,35 @@ public class AverageDelayPipeline {
 				}));
 		
 		WindowStats stats = new WindowStats();
-		stats.delay = airports //
-				.apply(event + ":airportdelay", ParDo.of(new DoFn<Airport, KV<Airport, Double>>() {
+		stats.delay = flights //
+				.apply(event + ":airportdelay", ParDo.of(new DoFn<Flight, KV<Airport, Double>>() {
 					@Override
 					public void processElement(ProcessContext c) throws Exception {
-						Airport airport = c.element();
-						c.output(KV.of(airport, airport.arr_delay)); // same as dep_delay
+						Flight stats = c.element();
+						c.output(KV.of(stats.airport, stats.delay)); // delay at airport
 					}
 				}))//
 				.apply(event + ":avgdelay", Mean.perKey());
 		
-		stats.timestamp = airports //
-				.apply(event + ":timestamps", ParDo.of(new DoFn<Airport, KV<Airport, String>>() {
+		stats.timestamp = flights //
+				.apply(event + ":timestamps", ParDo.of(new DoFn<Flight, KV<Airport, String>>() {
 					@Override
 					public void processElement(ProcessContext c) throws Exception {
-						Airport airport = c.element();
-						c.output(KV.of(airport, airport.timestamp));
+						Flight stats = c.element();
+						c.output(KV.of(stats.airport, stats.timestamp));
 					}
 				}))//
 				.apply(event + ":lastTimeStamp", Max.perKey());
+		
+		stats.num_flights = flights //
+				.apply(event + ":numflights", ParDo.of(new DoFn<Flight, KV<Airport, Integer>>() {
+					@Override
+					public void processElement(ProcessContext c) throws Exception {
+						Flight stats = c.element();
+						c.output(KV.of(stats.airport, 1));
+					}
+				}))//
+				.apply(event + ":total", Sum.integersPerKey());
 		
 		return stats;
 	}
