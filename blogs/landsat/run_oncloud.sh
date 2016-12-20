7cfa4f49f791bbded86299ff86304830bb377a23
Lak Lakshmanan <vlakshmanan@google.com>
2016-12-20 09:09:46 -0800

    add flag to enable autoscaling

--- blogs/landsat/run_oncloud.sh
+++ blogs/landsat/run_oncloud.sh
@@ -12,6 +12,7 @@ gsutil -m rm -rf gs://$BUCKET/landsat/output
     --temp_location=gs://$BUCKET/landsat/staging \
     --index_file=gs://cloud-training-demos/landsat/2015index.txt.gz \
     --max_num_workers=10 \
+    --autoscaling_algorithm=THROUGHPUT_BASED \
     --output_file=gs://$BUCKET/landsat/output/scenes.txt \
     --output_dir=gs://$BUCKET/landsat/output \
     --job_name=monthly-landsat \
