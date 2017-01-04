e927d3d58c3b4a24ea24e3c533767efc9de13011
Lak Lakshmanan <vlakshmanan@google.com>
2017-01-04 10:35:19 -0800

    to accompany blog post

---
+++ blogs/gcloudprojects/create_projects.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]; then
+   echo "Usage:  ./create_projects.sh  project-prefix  email1 [email2 [email3 ...]]]"
+   echo "   eg:  ./create_projects.sh  learnml-20170106  somebody@gmail.com someother@gmail.com"
+   exit
+fi
+
+gcloud components update
+gcloud components install alpha
+
+PROJECT_PREFIX=$1
+shift
+EMAILS=$@
+
+for EMAIL in $EMAILS; do
+   PROJECT_ID=$(echo "${PROJECT_PREFIX}-${EMAIL}" | sed 's/@/-/g' | sed 's/\./-/g' | cut -c 1-30)
+   echo "Creating project $PROJECT_ID for $EMAIL ... "
+
+   gcloud alpha projects create $PROJECT_ID
+   gcloud alpha projects get-iam-policy $PROJECT_ID --format=json > iam.json.orig
+   cat iam.json.orig | sed s'/"bindings": \[/"bindings": \[ \{"members": \["user:'$EMAIL'"\],"role": "roles\/editor"\},/g' > iam.json.new
+   gcloud alpha projects set-iam-policy $PROJECT_ID iam.json.new
+
+done
