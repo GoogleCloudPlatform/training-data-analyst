da22d84c17f7b604fcf1710aa2dedb3b173e089d
Lak Lakshmanan <vlakshmanan@google.com>
2017-01-16 23:13:26 -0800

    multithreaded delete

--- flights-data-analysis/06_dataproc/submit_pig.sh
+++ flights-data-analysis/06_dataproc/submit_pig.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gsutil rm -r gs://cloud-training-demos-ml/flights/pigoutput
+gsutil -m rm -r gs://cloud-training-demos-ml/flights/pigoutput
 gcloud dataproc jobs submit pig --cluster ch6cluster --file $*
