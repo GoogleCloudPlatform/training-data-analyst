8d6caf5e66fa639e498dc515d4cdef634791270e
Lak Lakshmanan <vlakshmanan@google.com>
2017-01-21 23:13:09 -0800

    Submittable script

--- flights-data-analysis/07_sparkml/logistic.py
+++ flights-data-analysis/07_sparkml/logistic.py
@@ -1,42 +1,73 @@
 from pyspark.mllib.classification import LogisticRegressionWithLBFGS
 from pyspark.mllib.regression import LabeledPoint
-
-# these two lines bring in a variable 'sc' to imitate the PySpark shell
+from pyspark.sql import SparkSession
 from pyspark import SparkContext
+
 sc = SparkContext('local', 'logistic')
+spark = SparkSession \
+    .builder \
+    .appName("Logistic regression w/ Spark ML") \
+    .getOrCreate()
 
-# read in list of training days
-traindays = sc.textFile('gs://cloud-training-demos/flights/trainday.csv') \
-              .filter(lambda line: 'True' in line) \
-              .map(lambda line: line.split(',')) \
-              .map(lambda fields: fields[0])
-traindays = set(traindays.collect()) # for fast searching
+BUCKET='cloud-training-demos-ml'
 
-# read the data, filtering it to keep only traindays and non-cancels
-# the header is organically removed because FL_DATE is not a trainday
-#allfields = sc.textFile('gs://cloud-training-demos/flights/201501.csv') \
-allfields = sc.textFile('gs://cloud-training-demos/flights/2015*.csv') \
-           .map(lambda line : line.split(',')) \
-           .filter(lambda fields: fields[0] in traindays and \
-                                  fields[22] != '')
+# read dataset
+traindays = spark.read \
+    .option("header", "true") \
+    .csv('gs://cloud-training-demos-ml/flights/trainday.csv')
+traindays.createOrReplaceTempView('traindays')
 
-# these are the fields we'll use in the regression
-# format is LabeledPoint(label, [x1, x2, ...]) 
-flights = allfields.map(lambda fields: LabeledPoint(\
-              float(float(fields[22]) < 15), #ontime \
+from pyspark.sql.types import StringType, FloatType, StructType, StructField
+
+header = 'FL_DATE,UNIQUE_CARRIER,AIRLINE_ID,CARRIER,FL_NUM,ORIGIN_AIRPORT_ID,ORIGIN_AIRPORT_SEQ_ID,ORIGIN_CITY_MARKET_ID,ORIGIN,DEST_AIRPORT_ID,DEST_AIRPORT_SEQ_ID,DEST_CITY_MARKET_ID,DEST,CRS_DEP_TIME,DEP_TIME,DEP_DELAY,TAXI_OUT,WHEELS_OFF,WHEELS_ON,TAXI_IN,CRS_ARR_TIME,ARR_TIME,ARR_DELAY,CANCELLED,CANCELLATION_CODE,DIVERTED,DISTANCE,DEP_AIRPORT_LAT,DEP_AIRPORT_LON,DEP_AIRPORT_TZOFFSET,ARR_AIRPORT_LAT,ARR_AIRPORT_LON,ARR_AIRPORT_TZOFFSET,EVENT,NOTIFY_TIME'
+
+def get_structfield(colname):
+   if colname in ['ARR_DELAY', 'DEP_DELAY', 'DISTANCE', 'TAXI_OUT']:
+      return StructField(colname, FloatType(), True)
+   else:
+      return StructField(colname, StringType(), True)
+
+schema = StructType([get_structfield(colname) for colname in header.split(',')])
+
+
+inputs = 'gs://cloud-training-demos-ml/flights/tzcorr/all_flights-00000-*' # 1/30th
+#inputs = 'gs://cloud-training-demos-ml/flights/tzcorr/all_flights-*'  # FULL
+flights = spark.read\
+            .schema(schema)\
+            .csv(inputs)
+
+# this view can now be queried ...
+flights.createOrReplaceTempView('flights')
+
+
+# logistic regression
+trainquery = """
+SELECT
+  *
+FROM flights f
+JOIN traindays t
+ON f.FL_DATE == t.FL_DATE
+WHERE
+  t.is_train_day == 'True' AND
+  f.dep_delay IS NOT NULL AND 
+  f.arr_delay IS NOT NULL
+"""
+traindata = spark.sql(trainquery)
+
+def to_example(fields):
+  return LabeledPoint(\
+              float(fields['ARR_DELAY'] < 15), #ontime \
               [ \
-                  float(fields[15]), # DEP_DELAY \
-                  float(fields[16]), # TAXI_OUT \
-                  float(fields[26]), # DISTANCE \
-              ]))
+                  fields['DEP_DELAY'], # DEP_DELAY \
+                  fields['TAXI_OUT'], # TAXI_OUT \
+                  fields['DISTANCE'], # DISTANCE \
+              ])
 
-#flights.saveAsTextFile('gs://cloud-training-demos/flights/sparkoutput/train')
+examples = traindata.rdd.map(to_example)
+lrmodel = LogisticRegressionWithLBFGS.train(examples, intercept=True)
+lrmodel.setThreshold(0.7)
 
-lrmodel = LogisticRegressionWithLBFGS.train(flights, intercept=True)
-print lrmodel.weights,lrmodel.intercept
+# save model
+MODEL_FILE='gs://' + BUCKET + '/flights/sparkmloutput/model'
+lrmodel.save(sc, MODEL_FILE)
 
-lrmodel.setThreshold(0.7) # cancel if prob-of-ontime < 0.7
-
-#print lrmodel.predict([36.0,12.0,594.0])
-
-lrmodel.save(sc, 'gs://cloud-training-demos/flights/sparkoutput/model')
--- flights-data-analysis/07_sparkml/submit_spark.sh
+++ flights-data-analysis/07_sparkml/submit_spark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gsutil -m rm -r gs://cloud-training-demos-ml/flights/sparkoutput
+gsutil -m rm -r gs://cloud-training-demos-ml/flights/sparkmloutput
 gcloud dataproc jobs submit pyspark --cluster ch6cluster $*
