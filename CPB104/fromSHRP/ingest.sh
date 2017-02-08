d7c8789fecea1fb9e71d0b904626313de2b2c9b3
lakshmanok <lak@vlakshman.com>
2017-02-08 17:37:06 +0000

    working program.

--- CPB104/fromSHRP/ingest.sh
+++ CPB104/fromSHRP/ingest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# curl http://shrp2archive.org/?attachment_id=896# -O SD_5min.csv
-gsutil cp gs://hsd_freeway_data/SD_5min.csv raw.csv
-python to_messages.py > messages.csv
-gsutil cp messages.csv gs://cloud-training-demos/sandiego/sensor_obs.csv
+gsutil cp gs://sd_freeway_data/SD_5min.csv raw.csv
+python to_messages.py
+gsutil cp messages.csv.gz gs://cloud-training-demos/sandiego/sensor_obs2008.csv.gz
