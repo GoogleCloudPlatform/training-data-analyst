b977bdb73802a3c0d88f699a4b53a6229dbfd7fa
Lak Lakshmanan <vlakshmanan@google.com>
2017-02-11 16:30:15 -0800

    based on new census sample without so much boilerplate

---
+++ CPB102/lab3a/taxifare/trainer/model.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from tensorflow.contrib import layers
+from tensorflow.contrib.learn.python.learn.utils import (
+    saved_model_export_utils, input_fn_utils)
+
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+CSV_COLUMNS = ['pickuplon','pickuplat','dropofflon','dropofflat','passengers', 'fare_amount']
+
+LABEL_COLUMN = 'fare_amount'
+
+DEFAULTS = [[-97], [40], [-97], [40], [1]]
+
+# These are the raw input columns, and will be provided for prediction also
+INPUT_COLUMNS = [
+    # sparse_column_with_keys
+
+    # sparse_column_with_hash_bucket
+
+    # real_valued_column
+    layers.real_valued_column('pickuplon'),
+    layers.real_valued_column('pickuplat'),
+    layers.real_valued_column('dropofflat'),
+    layers.real_valued_column('dropofflon'),
+    layers.real_valued_column('passengers'),
+]
+
+def build_estimator(model_dir, embedding_size, hidden_units):
+  """
+     Build an estimator starting from INPUT COLUMNS.
+     These include feature transformations and synthetic features.
+     The model is a wide-and-deep model.
+  """
+
+  # input columns
+  (plon, plat, dlon, dlat, pcount) = INPUT_COLUMNS 
+
+  # Sparse base columns.
+
+  # reusable transformations
+
+  # Wide columns and deep columns.
+  wide_columns = [
+      # feature crosses
+
+      # sparse columns with keys
+
+      # anything with a linear relationship
+      pcount 
+  ]
+
+  deep_columns = [
+      # embedding_column
+
+      # real_valued_column
+      plat, plon, dlat, dlon
+  ]
+
+  return tf.contrib.learn.DNNLinearCombinedClassifier(
+      model_dir=model_dir,
+      linear_feature_columns=wide_columns,
+      dnn_feature_columns=deep_columns,
+      dnn_hidden_units=hidden_units)
+
+
+serving_input_fn = input_fn_utils.build_parsing_serving_input_fn(
+    layers.create_feature_spec_for_parsing(INPUT_COLUMNS)
+)
+
+
+def generate_input_fn(filename, num_epochs=None, batch_size=512):
+  def _input_fn():
+    filename_queue = tf.train.string_input_producer(
+        [filename], num_epochs=num_epochs)
+    reader = tf.TextLineReader()
+    _, value = reader.read_up_to(filename_queue, num_records=batch_size)
+
+    value_column = tf.expand_dims(value, -1)
+
+    columns = tf.decode_csv(value_column, record_defaults=DEFAULTS)
+
+    features = dict(zip(CSV_COLUMNS, columns))
+
+    label = features.pop(LABEL_COLUMN, None)
+
+    return features, label
+
+  return _input_fn
+
