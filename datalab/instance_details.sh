6330d8a8f44b6d87214e514ca40abff39cebb87f
WeiweiVivianWang <weiweiwang528@gmail.com>
2017-02-14 14:27:30 -0500

    machine_type change

--- datalab/instance_details.sh
+++ datalab/instance_details.sh
@@ -3,5 +3,5 @@
 # Remove "_" from USER because "_" can not be used for instance name
 export INSTANCE_NAME=datalabvm-${USER//_/}
 export ZONE=us-central1-a
-export MACHINE_TYPE=n1-standard-1
+export MACHINE_TYPE=n1-highcpu-16
 
