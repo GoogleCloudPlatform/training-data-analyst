9f15ee3081f8e38d2bdd1d18f14d09e75dc0ce7c
Lak Lakshmanan <vlakshmanan@google.com>
2017-02-19 17:23:25 -0800

    save work

--- CPB102/lab3a/get_service_account.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PROJECT_ID=`gcloud config list project |grep project|perl -pe "s/.*= //"`
-AUTH_TOKEN=`gcloud auth print-access-token`
-SVC_ACCOUNT=`curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $AUTH_TOKEN" https://ml.googleapis.com/v1beta1/projects/$PROJECT_ID:getConfig | python -c "import json; import sys; response = json.load(sys.stdin); print response['serviceAccount']"`
-echo "$SVC_ACCOUNT"
