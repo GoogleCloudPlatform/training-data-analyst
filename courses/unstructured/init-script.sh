d4a782af75c6af6ea59ca237bfbc48e2462894da
Lak Lakshmanan <vlakshmanan@google.com>
2017-03-29 14:28:53 -0700

    no need for git clone. avoid errors ...

--- courses/unstructured/init-script.sh
+++ courses/unstructured/init-script.sh
@@ -5,11 +5,7 @@ apt-get update
 apt-get install -y python-pip
 pip install --upgrade google-api-python-client
 
-# git clone on Master
-USER=USER_NAME  # change this if neccessary
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 if [[ "${ROLE}" == 'Master' ]]; then
-  cd /home/$USER
-  git clone https://github.com/GoogleCloudPlatform/training-data-analyst
-  chown -R $USER training-data-analyst/
+   echo "Only on master node ..."
 fi
