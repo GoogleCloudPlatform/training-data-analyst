acba59ec3402e8779482f8da9b0db7cfcc3b7ac7
Lak Lakshmanan <vlakshmanan@google.com>
2017-03-29 21:34:30 -0700

    use the canonical one

--- courses/unstructured/create_mycluster.sh
+++ courses/unstructured/create_mycluster.sh
@@ -6,7 +6,5 @@ gcloud dataproc clusters create my-cluster --zone us-central1-a \
 	--master-machine-type n1-standard-1 --master-boot-disk-size 50 \
 	--num-workers 2 --worker-machine-type n1-standard-1 \
 	--worker-boot-disk-size 50 --network=default \
-        --initialization-actions=gs://$BUCKET/unstructured/init-script.sh,gs://$BUCKET/unstructured/datalab.sh
+        --initialization-actions=gs://$BUCKET/unstructured/init-script.sh,gs://dataproc-initialization-actions/datalab/datalab.sh
 
-
-#        --initialization-actions=gs://$BUCKET/unstructured/init-script.sh,gs://dataproc-initialization-actions/datalab/datalab.sh
