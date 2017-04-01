dd9a97870d27a672ac922dba9ee9185b79dbac41
Lak Lakshmanan <vlakshmanan@google.com>
2017-04-01 10:43:01 -0700

    add code to create overlapping sequences from a very long sequence

--- blogs/timeseries/simplernn/trainer/model.py
+++ blogs/timeseries/simplernn/trainer/model.py
@@ -35,8 +35,12 @@ N_INPUTS = SEQ_LEN - N_OUTPUTS
 def read_dataset(filename, mode=tf.contrib.learn.ModeKeys.TRAIN):  
   def _input_fn():
     num_epochs = 100 if mode == tf.contrib.learn.ModeKeys.TRAIN else 1
+
+    # could be a path to one file or a file pattern.
+    input_file_names = tf.train.match_filenames_once(filename)
     filename_queue = tf.train.string_input_producer(
-        [filename], num_epochs=num_epochs, shuffle=True)
+        input_file_names, num_epochs=num_epochs, shuffle=True)
+
     reader = tf.TextLineReader()
     _, value = reader.read_up_to(filename_queue, num_records=BATCH_SIZE)
 
