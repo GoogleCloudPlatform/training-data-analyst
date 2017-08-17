b2268dcd3176ec8d094290e1ae66a8fe79f0d870
Nigel Armstrong <nigel@peko.im>
2017-08-17 18:16:11 +0100

    Initial check in of datastore, cloudstorage, firebase, and pubsub-languageapi-spanner labs

---
+++ courses/developingapps/cloudstorage/start/server/gcp/cloudstorage.js
@@ -0,0 +1,49 @@
+// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict';
+
+const config = require('../config');
+
+
+
+// Express middleware that will automatically pass uploads to Cloud Storage.
+// req.file is processed and will have a new property:
+// * ``cloudStoragePublicUrl`` the public url to the object.
+// [START process]
+function sendUploadToGCS (req, res, next) {
+  if (!req.file) {
+    return next();
+  }
+
+  const oname = Date.now() + req.file.originalname;
+  
+}
+// [END process]
+
+// Multer handles parsing multipart/form-data requests.
+// This instance is configured to store images in memory.
+// This makes it straightforward to upload to Cloud Storage.
+// [START multer]
+const Multer = require('multer');
+const multer = Multer({
+  storage: Multer.MemoryStorage,
+  limits: {
+    fileSize: 40 * 1024 * 1024 // no larger than 40mb
+  }
+});
+// [END multer]
+
+module.exports = {
+  sendUploadToGCS,
+  multer
+};
---
+++ courses/developingapps/datastore/start/server/public/client/app/app.js
@@ -0,0 +1,5 @@
+(function () {
+
+    var app = angular.module('qiqApp', ['ui.bootstrap', 'qiqQuiz']);
+
+})();
