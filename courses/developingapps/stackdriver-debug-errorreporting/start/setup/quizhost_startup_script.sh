e96952eb68d1a5d297bf1da5be3ec2460721fb00
Nigel Armstrong <nigel@peko.im>
2017-08-22 11:50:40 +0100

    Added logging to compute engine startup script

--- courses/developingapps/stackdriver-debug-errorreporting/start/setup/quizhost_startup_script.sh
+++ courses/developingapps/stackdriver-debug-errorreporting/start/setup/quizhost_startup_script.sh
@@ -1,5 +1,6 @@
-# Installs nodejs, downloads source from git, installs ESP, runs app
+# Installs nodejs and logging agent
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
