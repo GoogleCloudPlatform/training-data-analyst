95a0f9877754443c7f135ac3b636dcf0daeb0bf5
Nigel Armstrong <nigel@peko.im>
2017-08-23 19:58:25 +0100

    Added dev machine lab logging statements

--- courses/developingapps/devenv/list-gce-instances.js
+++ courses/developingapps/devenv/list-gce-instances.js
@@ -16,6 +16,8 @@ const Compute = require('@google-cloud/compute');
 // Get a compute client
 const compute = Compute();
 
+console.log('Listing VMs in the project:');
+
 // Get the VM instances and display them
 compute.getVMs().then(([vms]) => {
     vms.forEach(console.log);
--- courses/developingapps/devenv/server/app.js
+++ courses/developingapps/devenv/server/app.js
@@ -12,7 +12,10 @@
 // limitations under the License.
 
 const http = require('http');
-
+console.log('Server starting...');
 http.createServer((req, res) => {
+    console.log('Request received...');
     res.end('Hello GCP dev!');
-}).listen(80);
+}).listen(80, () => {
+    console.log('started.');
+});
