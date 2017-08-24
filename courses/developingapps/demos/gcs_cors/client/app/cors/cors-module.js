3f321f818ed6fdc6bd3fc5dc1c648edb13341d55
Nigel Armstrong <nigel@peko.im>
2017-08-24 18:45:12 +0100

    Unsaved...

--- courses/developingapps/demos/gcs_cors/client/app/cors/cors-module.js
+++ courses/developingapps/demos/gcs_cors/client/app/cors/cors-module.js
@@ -12,18 +12,7 @@
 // limitations under the License.
 (function () {
 
-    var module = angular.module('qiqQuiz', ['ngRoute']);
-
-    module.config(['$routeProvider', config]);
-
-
-    function config($routeProvider) {
-        $routeProvider.when('/quiz/:name', {
-            controller: 'QuizController',
-            controllerAs: 'qc',
-            templateUrl: 'app/quizzes/quiz-template.html'
-        });
-    }
+    var module = angular.module('gcsCors');
 
 
 })();
