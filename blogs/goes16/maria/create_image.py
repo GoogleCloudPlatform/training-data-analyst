e4dd8ef1e0da17287143f7f882e856c4dd8c8ef7
Lak Lakshmanan <vlakshmanan@google.com>
2017-12-12 14:31:11 -0800

    use pyresample for area

--- blogs/goes16/maria/create_image.py
+++ blogs/goes16/maria/create_image.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
+from pyresample import geometry
 from satpy.scene import Scene
 from datetime import datetime
 import subprocess
 import satpy
 import shutil,os
 
-filename='MARIA.csv'
 tmpdir = 'tmp'
 outdir = 'image'
 shutil.rmtree(tmpdir)
 os.mkdirs(outdir)
-with open(filename, 'r') as ifp:
+with open('MARIA.csv', 'r') as ifp:
  for line in ifp:
    fields = line.split(',')
    # print '***'.join(fields)
@@ -19,6 +19,12 @@ with open(filename, 'r') as ifp:
    dayno = dt.timetuple().tm_yday
    lat = float(fields[8])
    lon = float(fields[9])
+
+   # make up the grid to resample into
+   lats = np.arange(lat-1,lat+1,0.01) # approx 1km resolution, 200km extent
+   lons = np.arange(lon-1,lon+1,0.01) # approx 1km resolution, 200km extent
+   lons, lats = np.meshgrid(lons, lats)
+   grid_def = geometry.GridDefinition(lons=lons, lats=lats)
 
    # copy 11-micron band (C14) to local disk
    # See: https://www.goes-r.gov/education/ABI-bands-quick-info.html
@@ -30,9 +36,10 @@ with open(filename, 'r') as ifp:
 
    # create image
    scene = Scene(filenames=outfile, reader="abi_l1b")
-   scene.resample("eurol")  #?? how to resample to area around (lat,lon)??
+   scene.resample(grid_def)
    outfile = '{}/ir_{}{}{}.png'.format(outdir, dt.year, dayno, dt.hour)
    scene.save_dataset('overview', outfile)
 
    # cleanup
    shutil.rmtree(tmpdir)
+   exit
--- blogs/goes16/maria/install.sh
+++ blogs/goes16/maria/install.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sudo apt-get install git
-git clone https://github.com/GoogleCloudPlatform/training-data-analyst
 sudo pip install satpy
