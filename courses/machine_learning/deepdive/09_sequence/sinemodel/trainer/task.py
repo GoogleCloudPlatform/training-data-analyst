6c82b16e9c5be0ac896f57a0cc49409d19c9aae2
Lak Lakshmanan <vlakshmanan@google.com>
2017-12-18 09:02:32 -0800

    Compute loss on last 1/3 of the sequence, but RMSE only on final value.

--- courses/machine_learning/deepdive/09_sequence/sinemodel/trainer/task.py
+++ courses/machine_learning/deepdive/09_sequence/sinemodel/trainer/task.py
@@ -31,17 +31,19 @@ def compute_errors(features, labels, predictions):
    if predictions.shape[1] == 1:
       loss = tf.losses.mean_squared_error(labels, predictions)
       rmse = tf.metrics.root_mean_squared_error(labels, predictions)
-      return loss, rmse, predictions
+      return loss, rmse
    else:
       # one prediction for every input in sequence
       # get 1-N of (x + label)
       labelsN = tf.concat([features[model.TIMESERIES_COL], labels], axis=1)
       labelsN = labelsN[:, 1:]
-      loss = tf.losses.mean_squared_error(labelsN, predictions)
+      # loss is computed from the last 1/3 of the series
+      N = (2 * model.N_INPUTS) // 3
+      loss = tf.losses.mean_squared_error(labelsN[:, N:], predictions[:, N:])
       # rmse is computed from last prediction and last label
       lastPred = predictions[:, -1]
       rmse = tf.metrics.root_mean_squared_error(labels, lastPred)
-      return loss, rmse, lastPred
+      return loss, rmse
 
 # create the inference model
 def sequence_regressor(features, labels, mode, params):
@@ -51,24 +53,31 @@ def sequence_regressor(features, labels, mode, params):
   predictions = model_func(features, mode, params)
 
   # 2. loss function, training/eval ops
-  if mode == tf.contrib.learn.ModeKeys.TRAIN or mode == tf.contrib.learn.ModeKeys.EVAL:
-     loss, rmse, predictions = compute_errors(features, labels, predictions)
+  loss = None
+  train_op = None
+  eval_metric_ops = None
+  if mode == tf.estimator.ModeKeys.TRAIN or mode == tf.estimator.ModeKeys.EVAL:
+     loss, rmse = compute_errors(features, labels, predictions)
 
-     # 2b. set up training operation and eval metrics
-     train_op = tf.contrib.layers.optimize_loss(
-         loss=loss,
-         global_step=tf.contrib.framework.get_global_step(),
-         learning_rate=params['learning_rate'],
-         optimizer="SGD")
+     if mode == tf.estimator.ModeKeys.TRAIN:
+        # this is needed for batch normalization, but has no effect otherwise
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+           # 2b. set up training operation
+           train_op = tf.contrib.layers.optimize_loss(
+              loss,
+              tf.train.get_global_step(),
+              learning_rate=params['learning_rate'],
+              optimizer="Adam")
+ 
+     # 2c. eval metric
      eval_metric_ops = {
       "rmse": rmse
      }
-  else:
-     loss = None
-     train_op = None
-     eval_metric_ops = None
 
   # 3. Create predictions
+  if predictions.shape[1] != 1:
+     predictions = predictions[:, -1] # last predicted value
   predictions_dict = {"predicted": predictions}
 
   # 4. return EstimatorSpec
@@ -94,10 +103,10 @@ def create_custom_estimator(output_dir, hparams):
 def generate_experiment_fn(output_dir, hparams):
  def experiment_fn(output_dir):
     get_train = model.read_dataset(hparams['train_data_paths'],
-                                   tf.contrib.learn.ModeKeys.TRAIN,
+                                   tf.estimator.ModeKeys.TRAIN,
                                    hparams['train_batch_size'])
     get_valid = model.read_dataset(hparams['eval_data_paths'],
-                                   tf.contrib.learn.ModeKeys.EVAL,
+                                   tf.estimator.ModeKeys.EVAL,
                                    1000)
     eval_freq = max(1, min(2000, hparams['train_steps']/5))
 
