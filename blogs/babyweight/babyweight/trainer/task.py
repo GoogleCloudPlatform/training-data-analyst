e21f91e4abe32aa8c26f653191759a548da3754c
Lak Lakshmanan <vlakshmanan@google.com>
2018-01-06 09:59:44 -0800

    core estimator

--- blogs/babyweight/babyweight/trainer/task.py
+++ blogs/babyweight/babyweight/trainer/task.py
@@ -75,4 +75,5 @@ if __name__ == '__main__':
   )
 
   # Run the training job
-  learn_runner.run(model.experiment_fn, output_dir)
+  #learn_runner.run(model.experiment_fn, output_dir)
+  model.train_and_evaluate(output_dir)
