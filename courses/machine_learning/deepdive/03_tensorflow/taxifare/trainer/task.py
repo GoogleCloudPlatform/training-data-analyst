d504c442c1a84ff4fc10695278216ceb685166c8
Lak Lakshmanan <vlakshmanan@google.com>
2018-01-26 10:20:39 -0800

    Migrate model to TensorFlow 1.4

--- courses/machine_learning/deepdive/03_tensorflow/taxifare/trainer/task.py
+++ courses/machine_learning/deepdive/03_tensorflow/taxifare/trainer/task.py
@@ -21,47 +21,6 @@ import os
 
 import model
 
-import tensorflow as tf
-from tensorflow.contrib.learn import Experiment
-from tensorflow.contrib.learn.python.learn import learn_runner
-from tensorflow.contrib.learn.python.learn.utils import (
-    saved_model_export_utils)
-
-
-def generate_experiment_fn(train_data_paths,
-                           eval_data_paths,
-                           format,
-                           num_epochs=None,
-                           train_batch_size=512,
-                           eval_batch_size=512,
-                           hidden_units=None,
-                           **experiment_args):
-
-  def _experiment_fn(output_dir):
-    input_fn = model.generate_csv_input_fn
-    train_input = input_fn(
-        train_data_paths, num_epochs=num_epochs, batch_size=train_batch_size)
-    eval_input = input_fn(
-        eval_data_paths, batch_size=eval_batch_size, mode=tf.contrib.learn.ModeKeys.EVAL)
-    return Experiment(
-        model.build_estimator(
-            output_dir,
-            hidden_units=hidden_units
-        ),
-        train_input_fn=train_input,
-        eval_input_fn=eval_input,
-        export_strategies=[saved_model_export_utils.make_export_strategy(
-            model.serving_input_fn,
-            default_output_alternative_key=None,
-            exports_to_keep=1
-        )],
-        eval_metrics=model.get_eval_metrics(),
-        #min_eval_frequency = 1000,  # change this to speed up training on large datasets
-        **experiment_args
-    )
-  return _experiment_fn
-
-
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   # Input Arguments
@@ -71,32 +30,15 @@ if __name__ == '__main__':
       required=True
   )
   parser.add_argument(
-      '--num_epochs',
-      help="""\
-      Maximum number of training data epochs on which to train.
-      If both --max-steps and --num-epochs are specified,
-      the training job will run for --max-steps or --num-epochs,
-      whichever occurs first. If unspecified will run for --max-steps.\
-      """,
-      type=int,
-  )
-  parser.add_argument(
       '--train_batch_size',
       help='Batch size for training steps',
       type=int,
       default=512
   )
   parser.add_argument(
-      '--eval_batch_size',
-      help='Batch size for evaluation steps',
-      type=int,
-      default=512
-  )
-  parser.add_argument(
       '--train_steps',
       help="""\
-      Steps to run the training job for. If --num-epochs is not specified,
-      this must be. Otherwise the training job will run indefinitely.\
+      Steps to run the training job for.\
       """,
       type=int
   )
@@ -139,14 +81,9 @@ if __name__ == '__main__':
   )
   parser.add_argument(
       '--min_eval_frequency',
-      help='Minimum number of training steps between evaluations',
-      default=1,
+      help='Seconds between evaluations',
+      default=300,
       type=int
-  )
-  parser.add_argument(
-      '--format',
-      help='Is the input data format csv or tfrecord?',
-      default='csv',
   )
 
   args = parser.parse_args()
@@ -156,7 +93,7 @@ if __name__ == '__main__':
   arguments.pop('job_dir', None)
   arguments.pop('job-dir', None)
 
-  output_dir = arguments.pop('output_dir')
+  output_dir = arguments['output_dir']
   # Append trial_id to path if we are doing hptuning
   # This code can be removed if you are not using hyperparameter tuning
   output_dir = os.path.join(
@@ -167,5 +104,5 @@ if __name__ == '__main__':
   )
 
   # Run the training job
-  learn_runner.run(generate_experiment_fn(**arguments), output_dir)
+  model.train_and_evaluate(arguments)
 
