090420764bd4bfe4f15cf7201a59dd4e80870443
Lak Lakshmanan <vlakshmanan@google.com>
2018-05-16 07:54:08 -0700

    no need for these any more, since datalab is available from command-line

--- datalab/cloudshell/datalab-cloudtraining/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /content
-if [ ! -d "training-data-analyst" ]; then
-   git clone http://github.com/GoogleCloudPlatform/training-data-analyst/
-fi
--- datalab/instance_details.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove "_" from USER because "_" can not be used for instance name
-export INSTANCE_NAME=datalabvm-${USER//_/}
-export ZONE=us-central1-a
-export MACHINE_TYPE=n1-standard-1
-
