384a49767775589867b830cfe0b9ad433f5f2ecd
Lak Lakshmanan <vlakshmanan@google.com>
2018-05-26 11:14:23 -0700

    To go with blog post

--- blogs/pandas-pvtkey/create_key.sh
+++ blogs/pandas-pvtkey/create_key.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+### This is a way to script out the creation of a key for your default
+### Compute Service Account.  You can get your project number from the
+### the GCP home page
+
 KEYFILE=trainer/privatekey.json
 PROJECTNUMBER=663413318684
 if [ ! -f $KEYFILE ]; then
