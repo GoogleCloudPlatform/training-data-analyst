1c1bc2dc0be38b6aaae81f9a30155fa9319bc2e2
Ameer Abbas <ameer00@gmail.com>
2018-06-13 13:40:48 -0700

    Update student-vm-startup.sh

--- bootcamps/advancedk8s/dm-setup/student-vm-startup.sh
+++ bootcamps/advancedk8s/dm-setup/student-vm-startup.sh
@@ -63,10 +63,7 @@ sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository 
-"deb [arch=amd64] https://download.docker.com/linux/debian 
-$(lsb_release -cs) 
-stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
 
