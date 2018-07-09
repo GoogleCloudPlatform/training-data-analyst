2966db5ea15c2ca7ce87ff0509d83a1fcda23daf
cjqian <cjqian@google.com>
2018-07-09 15:10:52 -0700

    Add hello world email examples

--- courses/data_analysis/deepdive/composer-exercises/hello_world_email_bonus.py
+++ courses/data_analysis/deepdive/composer-exercises/hello_world_email_bonus.py
@@ -1,9 +1,9 @@
 # This DAG is configured to print the date and sleep for 5 seconds.
 # However, it is configured to fail (see the invalid sleep bash_command)
 # and send an e-mail to your specified email on task failure.
-# We set retries to 0 to expedite this.
 
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
@@ -14,13 +14,13 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': YESTERDAY,
-    'email': ['YOUR E_MAIL HERE'],
+    'email': [ Variable.get('email') ],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 0,
 }
 
-with DAG('hello_world_email', default_args=default_args) as dag:
+with DAG('hello_world_email_bonus', default_args=default_args) as dag:
   t1 = BashOperator(task_id='print_date', bash_command='date', dag=dag)
   t2 = BashOperator(task_id='sleep', bash_command='sleepx 5', dag=dag)
   t1 >> t2
