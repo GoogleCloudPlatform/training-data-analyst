f9faa3a6ecf2a503ffbd8c995e3f7506ef54d698
Fredrik Averpil <fredrik@averpil.com>
2018-12-22 10:10:42 +0100

    Add working datastore.py with comments intact

--- courses/developingapps/python/datastore/start/quiz/gcp/datastore.py
+++ courses/developingapps/python/datastore/start/quiz/gcp/datastore.py
@@ -14,13 +14,13 @@
 
 # TODO: Import the os module
 
-
+import os
 
 # END TODO
 
 # TODO: Get the GCLOUD_PROJECT environment variable
 
-
+project_id = os.getenv('GCLOUD_PROJECT')
 
 # END TODO
 
@@ -28,7 +28,7 @@ from flask import current_app
 
 # TODO: Import the datastore module from the google.cloud package
 
-
+from google.cloud import datastore
 
 # END TODO
 
@@ -37,7 +37,7 @@ from flask import current_app
 # Pass through the Project ID you looked up from the
 # environment variable earlier
 
-
+datastore_client = datastore.Client(project_id)
 
 # END TODO
 
@@ -48,8 +48,19 @@ Returns a list of question entities for a given quiz
 - add in the entity key as the id property 
 - if redact is true, remove the correctAnswer property from each entity
 """
+
+# def list_entities(quiz='gcp', redact=True):
+#     return [{'quiz':'gcp', 'title':'Sample question', 'answer1': 'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D', 'correctAnswer': 1, 'author': 'Nigel'}]
 def list_entities(quiz='gcp', redact=True):
-    return [{'quiz':'gcp', 'title':'Sample question', 'answer1': 'A', 'answer2': 'B', 'answer3': 'C', 'answer4': 'D', 'correctAnswer': 1, 'author': 'Nigel'}]
+    query = datastore_client.query(kind='Question')
+    query.add_filter('quiz', '=', quiz)
+    results =list(query.fetch())
+    for result in results:
+        result['id'] = result.key.id
+    if redact:
+        for result in results:
+            del result['correctAnswer']
+    return results
 
 """
 Create and persist and entity for each question
@@ -59,33 +70,35 @@ There are two main ways of writing a key:
 2. Specify the kind and a unique string id
 """
 def save_question(question):
-# TODO: Create a key for a Datastore entity whose kind is Question
-    pass
+    # TODO: Create a key for a Datastore entity whose kind is Question
+    
+    key = datastore_client.key('Question')
     
 
-# END TODO
+    # END TODO
 
-# TODO: Create a Datastore entity object using the key
+    # TODO: Create a Datastore entity object using the key
 
-    
+    q_entity = datastore.Entity(key=key)
 
-# END TODO
+    # END TODO
 
-# TODO: Iterate over the form values supplied to the function
+    # TODO: Iterate over the form values supplied to the function
 
-    
+    for q_prop, q_val in question.iteritems():
 
-# END TODO
+        # END TODO
 
-# TODO: Assign each key and value to the Datastore entity
+        # TODO: Assign each key and value to the Datastore entity
 
-        
-
-# END TODO
+        q_entity[q_prop] = q_val
 
 
-# TODO: Save the entity
+    # END TODO
 
-    
 
-# END TODO
+    # TODO: Save the entity
+
+    datastore_client.put(q_entity)
+
+    # END TODO
