c65c53aa7a059417b19b42c6d41d01937bf3d147
A4q5PMrDW1d+vkfDYmdoLZOcKFKhZHgRZazKmBiqgKE= <vlakshmanan@google.com>
2019-02-14 20:55:46 -0800

    Update 3_install_sdk.sh

--- courses/machine_learning/deepdive/06_structured/pipelines/3_install_sdk.sh
+++ courses/machine_learning/deepdive/06_structured/pipelines/3_install_sdk.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pip3 install python-dateutil https://storage.googleapis.com/ml-pipeline/release/0.1.2/kfp.tar.gz --upgrade
+pip3 install python-dateutil https://storage.googleapis.com/ml-pipeline/release/0.1.3/kfp.tar.gz --upgrade
