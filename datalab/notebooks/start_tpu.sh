62195b47884b2f842f41c426bccfb9c8ad5dfbe4
Lak Lakshmanan <vlakshmanan@google.com>
2019-03-25 14:46:41 -0700

    launch scripts

---
+++ datalab/notebooks/start_tpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+INSTANCE_NAME=laktpu   # CHANGE THIS
+GCP_LOGIN_NAME=vlakshmanan@google.com  # CHANGE THIS
+TPU_NAME=$INSTANCE_NAME
+
+gcloud compute instances create $INSTANCE_NAME \
+--machine-type n1-standard-2 \
+--image-project deeplearning-platform-release \
+--image-family tf-1-13-cpu \
+--scopes cloud-platform \
+--metadata proxy-user-mail="${GCP_LOGIN_NAME}",\
+startup-script="echo export TPU_NAME=$TPU_NAME > /etc/profile.d/tpu-env.sh"
+
+gcloud compute tpus create $TPU_NAME \
+ --network default \
+ --range 10.240.1.0 \
+ --version 1.13
