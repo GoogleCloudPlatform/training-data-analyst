b8b52b05f1fe486cb54dadfa9fea26676a4a44a5
Lak Lakshmanan <vlakshmanan@google.com>
2019-03-29 18:00:56 -0700

    make sure to look in same zone

--- datalab/notebooks/start_cpu.sh
+++ datalab/notebooks/start_cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTANCE_NAME=tfcpu   # CHANGE THIS
+INSTANCE_NAME=tfxdemo   # CHANGE THIS
 GCP_LOGIN_NAME=vlakshmanan@google.com  # CHANGE THIS
 ZONE="us-west1-b" # CHANGE THIS
 INSTANCE_TYPE="n1-standard-4" # CHANGE THIS
@@ -22,7 +22,7 @@ gcloud compute instances create ${INSTANCE_NAME} \
 
 echo "Looking for Jupyter URL on $INSTANCE_NAME"
 while true; do
-   proxy=$(gcloud compute instances describe ${INSTANCE_NAME} 2> /dev/null | grep dot-datalab-vm)
+   proxy=$(gcloud compute instances describe --zone $ZONE ${INSTANCE_NAME} 2> /dev/null | grep dot-datalab-vm)
    if [ -z "$proxy" ]
    then
       echo -n "."
