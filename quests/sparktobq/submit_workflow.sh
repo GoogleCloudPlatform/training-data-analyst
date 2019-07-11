1bb3525d201e5eb6b275b6a83fa1631930f8f94a
A4q5PMrDW1d+vkfDYmdoLZOcKFKhZHgRZazKmBiqgKE= <lak@vlakshman.com>
2019-07-11 09:34:44 -0700

    make it easier to run

--- quests/sparktobq/submit_workflow.sh
+++ quests/sparktobq/submit_workflow.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-TEMPLATE=sparktobq
-MACHINE_TYPE=n1-highcpu-4
-CLUSTER=sparktobq
-BUCKET=cloud-training-demos-ml
 
-gcloud dataproc workflow-templates delete $TEMPLATE
-gcloud dataproc workflow-templates create $TEMPLATE
+if [ "$#" -ne 1 ]; then
+   echo "Usage:   ./submit_workflow.sh bucket-name"
+   exit
+fi
+
+TEMPLATE=sparktobq
+MACHINE_TYPE=n1-standard-4
+CLUSTER=sparktobq
+BUCKET=$1
+
+gsutil cp spark_analysis.py gs://$BUCKET/
+
+gcloud dataproc --quiet workflow-templates delete $TEMPLATE
+gcloud dataproc --quiet workflow-templates create $TEMPLATE
 
 # the things we need pip-installed on the cluster
 STARTUP_SCRIPT=gs://${BUCKET}/sparktobq/startup_script.sh
-echo "pip install --upgrade --quiet google-compute-engine google-cloud-storage" > /tmp/startup_script.sh
+echo "pip install --upgrade --quiet google-compute-engine google-cloud-storage matplotlib" > /tmp/startup_script.sh
 gsutil cp /tmp/startup_script.sh $STARTUP_SCRIPT
 
 # create new cluster for job
@@ -19,12 +27,12 @@ gcloud dataproc workflow-templates set-managed-cluster $TEMPLATE \
     --worker-machine-type $MACHINE_TYPE \
     --initialization-actions $STARTUP_SCRIPT \
     --num-workers 2 \
-    --image-version 1.14 \
+    --image-version 1.4 \
     --cluster-name $CLUSTER
 
 # steps in job
 gcloud dataproc workflow-templates add-job \
-  pyspark spark_analysis.py \
+  pyspark gs://$BUCKET/spark_analysis.py \
   --step-id create-report \
   --workflow-template $TEMPLATE \
   -- --bucket=$BUCKET
