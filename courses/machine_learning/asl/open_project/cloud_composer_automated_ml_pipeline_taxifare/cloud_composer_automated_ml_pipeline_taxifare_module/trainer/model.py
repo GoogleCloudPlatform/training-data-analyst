89fd7e9463940d67f584377821b657738b8362c9
Ryan Gillard <ryangillard@google.com>
2019-08-20 18:11:30 +0000

    add np back in

--- courses/machine_learning/asl/open_project/cloud_composer_automated_ml_pipeline_taxifare/cloud_composer_automated_ml_pipeline_taxifare_module/trainer/model.py
+++ courses/machine_learning/asl/open_project/cloud_composer_automated_ml_pipeline_taxifare/cloud_composer_automated_ml_pipeline_taxifare_module/trainer/model.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
