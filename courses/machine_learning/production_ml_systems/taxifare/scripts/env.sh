838c1fc6b61998c992160370a69e0cfeb42a799c
Benoit Dherin <dherin@google.com>
2019-09-25 18:36:07 +0100

    add a script to log into the container and one to test the container

--- courses/machine_learning/production_ml_systems/taxifare/scripts/env.sh
+++ courses/machine_learning/production_ml_systems/taxifare/scripts/env.sh
@@ -1,12 +1,12 @@
 # Replace with appropriate values.
-IMAGE_REPO_NAME=serverlessml_training_container
+IMAGE_NAME=taxifare_training_container
 BUCKET=ml-on-gcp-test
 REGION=us-west1
 
 # Generated.
-JOBID=${IMAGE_REPO_NAME}_$(date +%Y%m%d_%H%M%S)
+JOBID=${IMAGE_NAME}_$(date +%Y%m%d_%H%M%S)
 SCRIPTS_DIR=$(cd $(dirname $BASH_SOURCE) && pwd)
 PROJECT_DIR=$(dirname $SCRIPTS_DIR)
 PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME
+IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_NAME
 DOCKERFILE=$PROJECT_DIR/Dockerfile
