848035a7260ed6c97c6344e71b4e0ace14434aed
Benoit Dherin <dherin@google.com>
2019-09-28 21:51:12 +0100

    implemented KF pipeline taking now only 1 component: bq2gcs

---
+++ courses/machine_learning/production_ml_systems/pipelines/create_pipeline.py
@@ -0,0 +1,30 @@
+"""Generates the taxifare pipeline."""
+from os import path
+
+import kfp.compiler as compiler
+import kfp.components as comp
+import kfp.dsl as dsl
+import kfp.gcp as gcp
+
+
+HERE = path.abspath(path.dirname(__file__))
+COMPONENT_DIR = path.join(HERE, "components")
+BQ2GCS_YAML = path.join(COMPONENT_DIR, 'bq2gcs/component.yaml')
+PIPELINE_TAR = 'taxifare.tar.gz'
+
+
+@dsl.pipeline(
+    name='Taxifare',
+    description='Train a ml model to predict the taxi fare in NY')
+def pipeline(gcs_bucket_name='<bucket where data and model will be exported>'):
+
+  bq2gcs_op = comp.load_component_from_file(BQ2GCS_YAML)
+
+
+  cooc = bq2gcs_op(
+      input_bucket=gcs_bucket_name,
+  ).apply(gcp.use_gcp_secret('user-gcp-sa'))
+
+
+if __name__ == '__main__':
+  compiler.Compiler().compile(pipeline, PIPELINE_TAR, type_check=False)
