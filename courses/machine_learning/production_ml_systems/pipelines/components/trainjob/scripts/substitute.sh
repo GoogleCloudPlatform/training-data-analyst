056e76fe9863988c07f3ce10518ce992e13edc9c
Benoit Dherin <dherin@google.com>
2019-09-29 15:06:41 +0100

    fixed component.yaml template naming

--- courses/machine_learning/production_ml_systems/pipelines/components/trainjob/scripts/substitute.sh
+++ courses/machine_learning/production_ml_systems/pipelines/components/trainjob/scripts/substitute.sh
@@ -2,7 +2,7 @@
 
 . $(cd $(dirname $BASH_SOURCE) && pwd)/env.sh
 
-COMPONENT_YAML_TEMPL=$COMPONENT_DIR/component.yaml.template
+COMPONENT_YAML_TEMPL=$COMPONENT_DIR/component.template.yaml
 COMPONENT_YAML=$COMPONENT_DIR/component.yaml
 
 cat $COMPONENT_YAML_TEMPL | sed  "s/PROJECT/$PROJECT_ID/" > $COMPONENT_YAML
