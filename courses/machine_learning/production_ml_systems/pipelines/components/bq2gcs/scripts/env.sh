8e1cf58a5f830a5b951123fc49e17aa88d6df550
Benoit Dherin <dherin@google.com>
2019-09-29 09:42:31 +0100

    homogenize taxifare container image names

--- courses/machine_learning/production_ml_systems/pipelines/components/bq2gcs/scripts/env.sh
+++ courses/machine_learning/production_ml_systems/pipelines/components/bq2gcs/scripts/env.sh
@@ -2,6 +2,6 @@ SCRIPTS_DIR=$(cd $(dirname $BASH_SOURCE) && pwd)
 COMPONENT_DIR=$(dirname $SCRIPTS_DIR)
 COMPONENT_NAME=$(basename $COMPONENT_DIR)
 PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-IMAGE_NAME=taxifare-component-$COMPONENT_NAME
+IMAGE_NAME=taxifare-$COMPONENT_NAME
 IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_NAME
 DOCKERFILE=$COMPONENT_DIR/Dockerfile
