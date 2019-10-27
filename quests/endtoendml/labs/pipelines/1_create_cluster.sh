d849c6b329fe6f8d97e37c1e6af508ac40036c3c
Evan Jones <evanjones@google.com>
2019-10-27 15:33:56 -0700

    Adding remaining notebooks and artifacts to labs/
    TODO: create the TODOs as part of the labs notebooks

---
+++ quests/endtoendml/labs/pipelines/1_create_cluster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# CHANGE as needed. Note that not all zones support all gke versions!
+GKEVERSION=1.11.2-gke.26
+CLUSTERNAME=mykfp3
+ZONE=us-central1-a
+
+gcloud config set compute/zone $ZONE
+gcloud beta container clusters create $CLUSTERNAME \
+  --cluster-version $GKEVERSION --enable-autoupgrade \
+  --zone $ZONE \
+  --scopes cloud-platform \
+  --enable-cloud-logging \
+  --enable-cloud-monitoring \
+  --machine-type n1-standard-2 \
+  --enable-autoscaling --max-nodes=10 --min-nodes=3 
+
+# --num-nodes 4 
+#  --preemptible \
+#  --enable-autoprovisioning --max-cpu=40 --max-memory=1024 \
+
+kubectl create clusterrolebinding ml-pipeline-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+
+kubectl create clusterrolebinding pipelinerunnerbinding \
+  --clusterrole=cluster-admin \
+  --serviceaccount=kubeflow:pipeline-runner
