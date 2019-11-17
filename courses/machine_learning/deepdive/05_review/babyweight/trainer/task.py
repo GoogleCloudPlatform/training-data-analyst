a0a36b7e02e5dcc7bada757194cf3d7856f1c695
Benoit Dherin <dherin@google.com>
2019-11-17 18:26:10 +0900

    incorporated Mikes comments

--- courses/machine_learning/deepdive/05_review/babyweight/trainer/task.py
+++ courses/machine_learning/deepdive/05_review/babyweight/trainer/task.py
@@ -6,66 +6,70 @@ from . import model
 
 import tensorflow as tf
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--bucket",
-        help = "GCS path to data. We assume that data is in \
+        help="GCS path to data. We assume that data is in \
         gs://BUCKET/babyweight/preproc/",
-        required = True
+        required=True
     )
     parser.add_argument(
         "--output_dir",
-        help = "GCS location to write checkpoints and export models",
-        required = True
+        help="GCS location to write checkpoints and export models",
+        required=True
     )
     parser.add_argument(
         "--batch_size",
-        help = "Number of examples to compute gradient over.",
-        type = int,
-        default = 512
+        help="Number of examples to compute gradient over.",
+        type=int,
+        default=512
     )
     parser.add_argument(
         "--job-dir",
-        help = "this model ignores this field, but it is required by gcloud",
-        default = "junk"
+        help="this model ignores this field, but it is required by gcloud",
+        default="junk"
     )
     parser.add_argument(
         "--nnsize",
-        help = "Hidden layer sizes to use for DNN feature columns -- provide \
+        help="Hidden layer sizes to use for DNN feature columns -- provide \
         space-separated layers",
-        nargs = "+",
-        type = int,
+        nargs="+",
+        type=int,
         default=[128, 32, 4]
     )
     parser.add_argument(
         "--nembeds",
-        help = "Embedding size of a cross of n key real-valued parameters",
-        type = int,
-        default = 3
+        help="Embedding size of a cross of n key real-valued parameters",
+        type=int,
+        default=3
     )
     parser.add_argument(
         "--train_examples",
-        help = "Number of examples (in thousands) to run the training job over. \
-        If this is more than actual # of examples available, it cycles through them. \
-        So specifying 1000 here when you have only 100k examples makes this 10 epochs.",
-        type = int,
-        default = 5000
-    )    
+        help="Number of examples (in thousands) to run the training job over. \
+        If this is more than actual \
+        So specifying 1000 here when you have only 100k examples \
+        makes this 10 epochs.",
+        type=int,
+        default=5000
+    )
     parser.add_argument(
         "--pattern",
-        help = "Specify a pattern that has to be in input files. For example 00001-of \
+        help="Specify a pattern that has to be in input files. \
+        For example 00001-of \
         will process only one shard",
-        default = "of"
+        default="of"
     )
     parser.add_argument(
         "--eval_steps",
-        help = "Positive number of steps for which to evaluate model. Default to None, \
-        which means to evaluate until input_fn raises an end-of-input exception",
-        type = int,       
-        default = None
+        help="Positive number of steps for which to evaluate model. \
+        Default to None, which means to evaluate until \
+        input_fn raises an end-of-input exception",
+        type=int,
+        default=None
     )
-        
+
     # Parse arguments
     args = parser.parse_args()
     arguments = args.__dict__
@@ -75,13 +79,15 @@ if __name__ == "__main__":
 
     # Assign the arguments to the model variables
     output_dir = arguments.pop("output_dir")
-    model.BUCKET     = arguments.pop("bucket")
+    model.BUCKET = arguments.pop("bucket")
     model.BATCH_SIZE = arguments.pop("batch_size")
-    model.TRAIN_STEPS = (arguments.pop("train_examples") * 1000) / model.BATCH_SIZE
-    model.EVAL_STEPS = arguments.pop("eval_steps")    
-    print ("Will train for {} steps using batch_size={}".format(model.TRAIN_STEPS, model.BATCH_SIZE))
+    model.TRAIN_STEPS = (
+        arguments.pop("train_examples") * 1000) / model.BATCH_SIZE
+    model.EVAL_STEPS = arguments.pop("eval_steps")
+    print ("Will train for {} steps using batch_size={}".format(
+        model.TRAIN_STEPS, model.BATCH_SIZE))
     model.PATTERN = arguments.pop("pattern")
-    model.NEMBEDS= arguments.pop("nembeds")
+    model.NEMBEDS = arguments.pop("nembeds")
     model.NNSIZE = arguments.pop("nnsize")
     print ("Will use DNN size of {}".format(model.NNSIZE))
 
