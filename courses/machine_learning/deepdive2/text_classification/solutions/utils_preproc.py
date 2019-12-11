42184fc7bf05bf66f6e667f5c65b667700836ea9
Benoit Dherin <dherin@google.com>
2019-12-11 15:43:44 +0000

    pep8 utils_preproc.py

--- courses/machine_learning/deepdive2/text_classification/solutions/utils_preproc.py
+++ courses/machine_learning/deepdive2/text_classification/solutions/utils_preproc.py
@@ -1,21 +1,22 @@
 """Utilities for preprocessing natural language for machine learning"""
-
-import unicodedata
 import re
+import unicodedata
 
 import tensorflow as tf
 
+
 def unicode_to_ascii(s):
-        return ''.join(c for c in unicodedata.normalize('NFD', s)
-            if unicodedata.category(c) != 'Mn')
+    normalized = unicodedata.normalize('NFD', s)
+    return ''.join(c for c in normalized if unicodedata.category(c) != 'Mn')
+
 
 def preprocess_sentence(w):
     w = unicode_to_ascii(w.lower().strip())
 
     # creating a space between a word and the punctuation following it
     # eg: "he is a boy." => "he is a boy ."
-
     w = re.sub(r"([?.!,¿])", r" \1 ", w)
+
     w = re.sub(r'[" "]+', " ", w)
 
     # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
@@ -28,52 +29,55 @@ def preprocess_sentence(w):
     w = '<start> ' + w + ' <end>'
     return w
 
-def tokenize(lang,lang_tokenizer=None):
+
+def tokenize(lang, lang_tokenizer=None):
     """Given a list of sentences, return an integer representation
-    
+
     Arguments:
-    lang -- a python list of setences
+    lang -- a python list of sentences
     lang_tokenizer -- keras_preprocessing.text.Tokenizer, if None
         this will be created for you
-    
+
     Returns:
     tensor -- int tensor of shape (NUM_EXAMPLES,MAX_SENTENCE_LENGTH)
     lang_tokenizer -- keras_preprocessing.text.Tokenizer
     """
-    if lang_tokenizer==None:
+    if lang_tokenizer is None:
         lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
           filters='')
         lang_tokenizer.fit_on_texts(lang)
 
     tensor = lang_tokenizer.texts_to_sequences(lang)
 
-    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
-                                                         padding='post')
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(
+        tensor, padding='post')
 
     return tensor, lang_tokenizer
 
+
 def preprocess(sentences, tokenizer):
     """Preprocesses then tokenizes text
-    
+
     Arguments:
     sentences -- a python list of of strings
-    tokenizer -- keras_preprocessing.text.Tokenizer for mapping words to integers
-    
+    tokenizer -- Tokenizer for mapping words to integers
+
     Returns:
     tensor -- int tensor of shape (NUM_EXAMPLES,MAX_SENTENCE_LENGTH)
     lang_tokenizer -- keras_preprocessing.text.Tokenizer
     """
     sentences = [preprocess_sentence(sentence) for sentence in sentences]
-    tokens, _ = tokenize(sentences,tokenizer)
+    tokens, _ = tokenize(sentences, tokenizer)
     return tokens
 
-def int2word(tokenizer,int_sequence):
+
+def int2word(tokenizer, int_sequence):
     """Converts integer representation to natural language representation
-    
+
     Arguments:
     tokenizer -- keras_preprocessing.text.Tokenizer
     int_sequence -- an iterable or rank 1 tensor of integers
-    
+
     Returns list of string tokens
     """
-    return [tokenizer.index_word[t] if t!=0 else '' for t in int_sequence]
+    return [tokenizer.index_word[t] if t != 0 else '' for t in int_sequence]
