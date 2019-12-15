ee75d437036e3a04ccf99550ad4ec86b6c44206d
Lakshmanan, V <lakshmanok@gmail.com>
2019-12-14 22:39:42 -0800

    Handle both NASA and SEDAC data.

--- blogs/popdensity/README.md
+++ blogs/popdensity/README.md
@@ -1,1 +1,15 @@
 Accompanies the blog post  "Querying geographic raster data in BigQuery the brute force way"
+
+https://medium.com/google-cloud/querying-geographic-raster-data-in-bigquery-the-brute-force-way-1da46799d65f
+
+
+(1) First download data from either NASA or SEDAC
+
+
+(2) Then, run:
+      ./convert_to_geo.py --format=nasa --input popdensity.csv
+    OR
+      ./convert_to_geo.py --format=sedac --input gpw_v4_population_density*.asc
+
+(3) Then, run:
+      ./load_geo_into_bq.sh
--- blogs/popdensity/load_geo_into_bq.sh
+++ blogs/popdensity/load_geo_into_bq.sh
@@ -1,1 +1,26 @@
-bq load --source_format=NEWLINE_DELIMITED_JSON advdata.popdensity popdensity_geo.json.gz schema.json
+#!/bin/bash
+
+BUCKET=cloud-training-demos-ml   # change as needed
+
+FILE=popdensity_geo.json.gz
+GCSFILE=gs://$BUCKET/popdensity/$FILE
+
+gsutil -m cp $FILE $GCSFILE
+
+bq_safe_mk() {
+    dataset=$1
+    exists=$(bq ls -d | grep -w $dataset)
+    if [ -n "$exists" ]; then
+       echo "Not creating $dataset since it already exists"
+    else
+       echo "Creating $dataset"
+       bq mk $dataset
+    fi
+}
+
+bq_safe_mk advdata
+bq load --replace \
+   --source_format NEWLINE_DELIMITED_JSON \
+   --description "Population density" \
+   --clustering_fields tile \
+   advdata.popdensity $GCSFILE schema.json
