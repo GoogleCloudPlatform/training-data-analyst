72db672fa7794471e96455332e644eafcec54300
Damon A. Runion <48971473+damonarunion@users.noreply.github.com>
2019-12-30 16:23:26 -0600

    Add files via upload

--- courses/dw-pso/dataflow_python_examples/data_lake_to_mart_cogroupbykey.py
+++ courses/dw-pso/dataflow_python_examples/data_lake_to_mart_cogroupbykey.py
@@ -17,7 +17,7 @@ large BigQuery Table, joins in another dataset, and writes its contents to a
 BigQuery table.  
 """
 
-from __future__ import absolute_import
+
 import argparse
 import logging
 import os
@@ -206,8 +206,9 @@ class DataLakeToDataMartCGBK:
         """
         return orders_query
 
-    def add_account_details(self, (acct_number, data)):
+    def add_account_details(self, account_info):
         """This function performs the join of the two datasets."""
+        (acct_number, data) = account_info
         result = list(data['orders'])
         if not data['account_details']:
             logging.info('account details are empty')
