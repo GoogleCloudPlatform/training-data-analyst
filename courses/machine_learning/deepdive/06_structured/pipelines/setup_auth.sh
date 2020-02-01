abd9c0854fc377b86d7a3546392e9b6c6f647cca
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-01 23:55:57 +0000

    Updating to Hosted Pipelines

---
+++ courses/machine_learning/deepdive/06_structured/pipelines/setup_auth.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: ./setup_auth.sh service-account-name zone cluster namespace"
+    echo "     Get these values by visiting https://console.cloud.google.com/ai-platform/pipelines/clusters"
+    echo "  eg:  ./setup_auth.sh kfpdemo us-central1-a cluster-1 default"
+    exit
+fi
+
+PROJECT_ID=$(gcloud config get-value project)
+SA_NAME=$1
+ZONE=$2
+CLUSTER=$3
+NAMESPACE=$4
+
+
+# See: https://github.com/kubeflow/pipelines/blob/master/manifests/gcp_marketplace/guide.md#gcp-service-account-credentials
+
+gcloud container clusters get-credentials "$CLUSTER" --zone "$ZONE" --project "$PROJECT_ID"
+
+
+# Create service account
+gcloud iam service-accounts create $SA_NAME \
+       --display-name $SA_NAME --project "$PROJECT_ID"
+
+# Grant permissions to the service account by binding roles
+# These are rather broad; you might want to use narrower roles for your users
+for ROLE in roles/storage.admin roles/bigquery.admin roles/ml.admin roles/dataflow.admin; do
+  gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member=serviceAccount:$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com \
+    --role=$ROLE
+done
+
+# Create credential for the service account
+gcloud iam service-accounts keys create application_default_credentials.json --iam-account $SA_NAME@$PROJECT_ID.iam.gserviceaccount.com
+
+# Attempt to create a k8s secret. If already exists, override.
+kubectl create secret generic user-gcp-sa \
+  --from-file=user-gcp-sa.json=application_default_credentials.json \
+  -n $NAMESPACE --dry-run -o yaml  |  kubectl apply -f -
+  
+# remove private key file
+rm application_default_credentials.json
