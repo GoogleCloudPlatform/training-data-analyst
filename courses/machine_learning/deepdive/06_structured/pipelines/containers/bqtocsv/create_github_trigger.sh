52ca44eaaf026495098ce4e3c33b37ea19c32c24
Lakshmanan, V <lakshmanok@gmail.com>
2020-02-06 06:05:38 +0000

    relative to the home directory of repo

--- courses/machine_learning/deepdive/06_structured/pipelines/containers/bqtocsv/create_github_trigger.sh
+++ courses/machine_learning/deepdive/06_structured/pipelines/containers/bqtocsv/create_github_trigger.sh
@@ -4,7 +4,7 @@
 # In order to try this out, fork this repo into your personal GitHub account
 # Then, change the repo-owner to be your GitHub id
 
-DIR=$(pwd | sed 's/training-data-analyst/ /g' | awk '{print $2}')
+DIR=$(pwd | sed 's%training-data-analyst/% %g' | awk '{print $2}')
 echo $DIR
 
 gcloud beta builds triggers create github \
