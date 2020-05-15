3e1d66d0dfbc7738a7a023ae8aa566f4dc155a5c
Damon A. Runion <48971473+damonarunion@users.noreply.github.com>
2020-05-15 11:12:34 -0500

    remove wget

--- CPB100/lab2b/ingest.sh
+++ CPB100/lab2b/ingest.sh
@@ -4,7 +4,7 @@
 rm -f earthquakes.csv
 
 # download latest data from USGS
-wget http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv -O earthquakes.csv
+curl http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv -O earthquakes.csv
 
 # Copyright 2016 Google Inc.
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
