8c48574031f475d47207c2abd89e88c3f611b085
kalyanipande-tudip <kalyani.pande@tudip.com>
2020-06-03 18:54:32 +0530

    Update:Tensorflow Upgradation for train_deploy Notebook
    - Removed the instructions from notebook as required.
    - Added instructions in the model.py file as required.
    - Added instructions in task.py as required.

--- blogs/babyweight/babyweight/trainer/model.py
+++ blogs/babyweight/babyweight/trainer/model.py
@@ -14,140 +14,271 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
+import datetime
+import os
 import shutil
 import numpy as np
 import tensorflow as tf
+import hypertune
 
-tf.logging.set_verbosity(tf.logging.INFO)
+# Determine CSV, label, and key columns
+CSV_COLUMNS = ["weight_pounds",
+               "is_male",
+               "mother_age",
+               "plurality",
+               "gestation_weeks"]
+LABEL_COLUMN = "weight_pounds"
 
-BUCKET = None  # set from task.py
-PATTERN = 'of' # gets all files
-TRAIN_STEPS = 10000
-CSV_COLUMNS = 'weight_pounds,is_male,mother_age,plurality,gestation_weeks,key'.split(',')
-LABEL_COLUMN = 'weight_pounds'
-KEY_COLUMN = 'key'
-DEFAULTS = [[0.0], ['null'], [0.0], ['null'], [0.0], ['nokey']]
+# Set default values for each CSV column.
+# Treat is_male and plurality as strings.
+DEFAULTS = [[0.0], ["null"], [0.0], ["null"], [0.0]]
 
-def read_dataset(prefix, pattern, batch_size=512):
-    # use prefix to create filename
-    filename = 'gs://{}/babyweight/preproc/{}*{}*'.format(BUCKET, prefix, pattern)
-    if prefix == 'train':
-        mode = tf.estimator.ModeKeys.TRAIN
-        num_epochs = None # indefinitely
-    else:
-        mode = tf.estimator.ModeKeys.EVAL
-        num_epochs = 1 # end-of-input after this
-    
-    # the actual input function passed to TensorFlow
-    def _input_fn():
-        # could be a path to one file or a file pattern.
-        input_file_names = tf.train.match_filenames_once(filename)
-        filename_queue = tf.train.string_input_producer(
-            input_file_names, shuffle=True, num_epochs=num_epochs)
- 
-        # read CSV
-        reader = tf.TextLineReader()
-        _, value = reader.read_up_to(filename_queue, num_records=batch_size)
-        if mode == tf.estimator.ModeKeys.TRAIN:
-          value = tf.train.shuffle_batch([value], batch_size, capacity=10*batch_size, 
-                                         min_after_dequeue=batch_size, enqueue_many=True, 
-                                         allow_smaller_final_batch=False)
-        value_column = tf.expand_dims(value, -1)
-        columns = tf.decode_csv(value_column, record_defaults=DEFAULTS)
-        features = dict(zip(CSV_COLUMNS, columns))
-        features.pop(KEY_COLUMN)
-        label = features.pop(LABEL_COLUMN)
-        return features, label
-  
-    return _input_fn
 
-def get_wide_deep():
-    # define column types
-    is_male,mother_age,plurality,gestation_weeks = \
-        [\
-            tf.feature_column.categorical_column_with_vocabulary_list('is_male', 
-                        ['True', 'False', 'Unknown']),
-            tf.feature_column.numeric_column('mother_age'),
-            tf.feature_column.categorical_column_with_vocabulary_list('plurality',
-                        ['Single(1)', 'Twins(2)', 'Triplets(3)',
-                         'Quadruplets(4)', 'Quintuplets(5)','Multiple(2+)']),
-            tf.feature_column.numeric_column('gestation_weeks')
-        ]
+def features_and_labels(row_data):
+    """Splits features and labels from feature dictionary.
 
-    # discretize
-    age_buckets = tf.feature_column.bucketized_column(mother_age, 
-                        boundaries=np.arange(15,45,1).tolist())
-    gestation_buckets = tf.feature_column.bucketized_column(gestation_weeks, 
-                        boundaries=np.arange(17,47,1).tolist())
-      
-    # sparse columns are wide 
-    wide = [is_male,
-            plurality,
-            age_buckets,
-            gestation_buckets]
-    
-    # feature cross all the wide columns and embed into a lower dimension
-    crossed = tf.feature_column.crossed_column(wide, hash_bucket_size=20000)
-    embed = tf.feature_column.embedding_column(crossed, 3)
-    
-    # continuous columns are deep
-    deep = [mother_age,
-            gestation_weeks,
-            embed]
-    return wide, deep
+    Args:
+        row_data: Dictionary of CSV column names and tensor values.
+    Returns:
+        Dictionary of feature tensors and label tensor.
+    """
+    label = row_data.pop(LABEL_COLUMN)
 
-def serving_input_fn():
-    feature_placeholders = {
-        'is_male': tf.placeholder(tf.string, [None]),
-        'mother_age': tf.placeholder(tf.float32, [None]),
-        'plurality': tf.placeholder(tf.string, [None]),
-        'gestation_weeks': tf.placeholder(tf.float32, [None])
+    return row_data, label  # features, label
+
+
+def load_dataset(pattern, batch_size=1, mode='eval'):
+    """Loads dataset using the tf.data API from CSV files.
+
+    Args:
+        pattern: str, file pattern to glob into list of files.
+        batch_size: int, the number of examples per batch.
+        mode: 'train' | 'eval' to determine if training or evaluating.
+    Returns:
+        `Dataset` object.
+    """
+    print("mode = {}".format(mode))
+    # Make a CSV dataset
+    dataset = tf.data.experimental.make_csv_dataset(
+        file_pattern=pattern,
+        batch_size=batch_size,
+        column_names=CSV_COLUMNS,
+        column_defaults=DEFAULTS)
+
+    # Map dataset to features and label
+    dataset = dataset.map(map_func=features_and_labels)  # features, label
+
+    # Shuffle and repeat for training
+    if mode == 'train':
+        dataset = dataset.shuffle(buffer_size=1000).repeat()
+
+    # Take advantage of multi-threading; 1=AUTOTUNE
+    dataset = dataset.prefetch(buffer_size=1)
+
+    return dataset
+
+
+def create_input_layers():
+    """Creates dictionary of input layers for each feature.
+
+    Returns:
+        Dictionary of `tf.Keras.layers.Input` layers for each feature.
+    """
+    deep_inputs = {
+        colname: tf.keras.layers.Input(
+            name=colname, shape=(), dtype="float32")
+        for colname in ["mother_age", "gestation_weeks"]
     }
-    features = {
-        key: tf.expand_dims(tensor, -1)
-        for key, tensor in feature_placeholders.items()
+
+    wide_inputs = {
+        colname: tf.keras.layers.Input(
+            name=colname, shape=(), dtype="string")
+        for colname in ["is_male", "plurality"]
     }
-    return tf.estimator.export.ServingInputReceiver(features, feature_placeholders)
 
-from tensorflow.contrib.learn.python.learn.utils import saved_model_export_utils
+    inputs = {**wide_inputs, **deep_inputs}
 
-def experiment_fn(output_dir):
-    wide, deep = get_wide_deep()
-    return tf.contrib.learn.Experiment(
-        tf.estimator.DNNLinearCombinedRegressor(model_dir=output_dir,
-                                                    linear_feature_columns=wide,
-                                                    dnn_feature_columns=deep,
-                                                    dnn_hidden_units=[64, 32]),
-        train_input_fn=read_dataset('train', PATTERN),
-        eval_input_fn=read_dataset('eval', PATTERN),
-        export_strategies=[saved_model_export_utils.make_export_strategy(
-            serving_input_fn,
-            default_output_alternative_key=None,
-            exports_to_keep=1
-        )],
-        train_steps=TRAIN_STEPS,
-        eval_steps=None
-    )
+    return inputs
 
 
-def train_and_evaluate(output_dir):
-    wide, deep = get_wide_deep()
-    estimator = tf.estimator.DNNLinearCombinedRegressor(
-                         model_dir=output_dir,
-                         linear_feature_columns=wide,
-                         dnn_feature_columns=deep,
-                         dnn_hidden_units=[64, 32])
-    train_spec=tf.estimator.TrainSpec(
-                         input_fn=read_dataset('train', PATTERN),
-                         max_steps=TRAIN_STEPS)
-    exporter = tf.estimator.LatestExporter('exporter',serving_input_fn)
-    eval_spec=tf.estimator.EvalSpec(
-                         input_fn=read_dataset('eval', PATTERN),
-                         steps=None,
-                         exporters=exporter)
-    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+def categorical_fc(name, values):
+    """Helper function to wrap categorical feature by indicator column.
 
+    Args:
+        name: str, name of feature.
+        values: list, list of strings of categorical values.
+    Returns:
+        Categorical and indicator column of categorical feature.
+    """
+    cat_column = tf.feature_column.categorical_column_with_vocabulary_list(
+            key=name, vocabulary_list=values)
+    ind_column = tf.feature_column.indicator_column(
+        categorical_column=cat_column)
+
+    return cat_column, ind_column
+
+
+def create_feature_columns(nembeds):
+    """Creates wide and deep dictionaries of feature columns from inputs.
+
+    Args:
+        nembeds: int, number of dimensions to embed categorical column down to.
+    Returns:
+        Wide and deep dictionaries of feature columns.
+    """
+    deep_fc = {
+        colname: tf.feature_column.numeric_column(key=colname)
+        for colname in ["mother_age", "gestation_weeks"]
+    }
+    wide_fc = {}
+    is_male, wide_fc["is_male"] = categorical_fc(
+        "is_male", ["True", "False", "Unknown"])
+    plurality, wide_fc["plurality"] = categorical_fc(
+        "plurality", ["Single(1)", "Twins(2)", "Triplets(3)",
+                      "Quadruplets(4)", "Quintuplets(5)", "Multiple(2+)"])
+
+    # Bucketize the float fields. This makes them wide
+    age_buckets = tf.feature_column.bucketized_column(
+        source_column=deep_fc["mother_age"],
+        boundaries=np.arange(15, 45, 1).tolist())
+    wide_fc["age_buckets"] = tf.feature_column.indicator_column(
+        categorical_column=age_buckets)
+
+    gestation_buckets = tf.feature_column.bucketized_column(
+        source_column=deep_fc["gestation_weeks"],
+        boundaries=np.arange(17, 47, 1).tolist())
+    wide_fc["gestation_buckets"] = tf.feature_column.indicator_column(
+        categorical_column=gestation_buckets)
+
+    # Cross all the wide columns, have to do the crossing before we one-hot
+    crossed = tf.feature_column.crossed_column(
+        keys=[age_buckets, gestation_buckets],
+        hash_bucket_size=1000)
+    deep_fc["crossed_embeds"] = tf.feature_column.embedding_column(
+        categorical_column=crossed, dimension=nembeds)
+
+    return wide_fc, deep_fc
+
+
+def get_model_outputs(wide_inputs, deep_inputs, dnn_hidden_units):
+    """Creates model architecture and returns outputs.
+
+    Args:
+        wide_inputs: Dense tensor used as inputs to wide side of model.
+        deep_inputs: Dense tensor used as inputs to deep side of model.
+        dnn_hidden_units: List of integers where length is number of hidden
+            layers and ith element is the number of neurons at ith layer.
+    Returns:
+        Dense tensor output from the model.
+    """
+    # Hidden layers for the deep side
+    layers = [int(x) for x in dnn_hidden_units]
+    deep = deep_inputs
+    for layerno, numnodes in enumerate(layers):
+        deep = tf.keras.layers.Dense(
+            units=numnodes,
+            activation="relu",
+            name="dnn_{}".format(layerno+1))(deep)
+    deep_out = deep
+
+    # Linear model for the wide side
+    wide_out = tf.keras.layers.Dense(
+        units=10, activation="relu", name="linear")(wide_inputs)
+
+    # Concatenate the two sides
+    both = tf.keras.layers.concatenate(
+        inputs=[deep_out, wide_out], name="both")
+
+    # Final output is a linear activation because this is regression
+    output = tf.keras.layers.Dense(
+        units=1, activation="linear", name="weight")(both)
+
+    return output
+
+
+def rmse(y_true, y_pred):
+    """Calculates RMSE evaluation metric.
+
+    Args:
+        y_true: tensor, true labels.
+        y_pred: tensor, predicted labels.
+    Returns:
+        Tensor with value of RMSE between true and predicted labels.
+    """
+    return tf.sqrt(tf.reduce_mean(tf.square(y_pred - y_true)))
+
+
+def build_wide_deep_model(dnn_hidden_units=[64, 32], nembeds=3):
+    """Builds wide and deep model using Keras Functional API.
+
+    Returns:
+        `tf.keras.models.Model` object.
+    """
+    # Create input layers
+    inputs = create_input_layers()
+
+    # Create feature columns for both wide and deep
+    wide_fc, deep_fc = create_feature_columns(nembeds)
+
+    # The constructor for DenseFeatures takes a list of numeric columns
+    # The Functional API in Keras requires: LayerConstructor()(inputs)
+    wide_inputs = tf.keras.layers.DenseFeatures(
+        feature_columns=wide_fc.values(), name="wide_inputs")(inputs)
+    deep_inputs = tf.keras.layers.DenseFeatures(
+        feature_columns=deep_fc.values(), name="deep_inputs")(inputs)
+
+    # Get output of model given inputs
+    output = get_model_outputs(wide_inputs, deep_inputs, dnn_hidden_units)
+
+    # Build model and compile it all together
+    model = tf.keras.models.Model(inputs=inputs, outputs=output)
+    model.compile(optimizer="adam", loss="mse", metrics=[rmse, "mse"])
+
+    return model
+
+
+def train_and_evaluate(args):
+    model = build_wide_deep_model(args["nnsize"], args["nembeds"])
+    print("Here is our Wide-and-Deep architecture so far:\n")
+    print(model.summary())
+
+    trainds = load_dataset(
+        args["train_data_path"],
+        args["batch_size"],
+        'train')
+
+    evalds = load_dataset(
+        args["eval_data_path"], 1000, 'eval')
+    if args["eval_steps"]:
+        evalds = evalds.take(count=args["eval_steps"])
+
+    num_batches = args["batch_size"] * args["num_epochs"]
+    steps_per_epoch = args["train_examples"] // num_batches
+
+    checkpoint_path = os.path.join(args["output_dir"], "checkpoints/babyweight")
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path, verbose=1, save_weights_only=True)
+
+    history = model.fit(
+        trainds,
+        validation_data=evalds,
+        epochs=args["num_epochs"],
+        steps_per_epoch=steps_per_epoch,
+        verbose=2,  # 0=silent, 1=progress bar, 2=one line per epoch
+        callbacks=[cp_callback])
+
+    EXPORT_PATH = os.path.join(
+        args["output_dir"], datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    tf.saved_model.save(
+        obj=model, export_dir=EXPORT_PATH)  # with default serving function
+
+    hp_metric = history.history['val_rmse'][-1]
+
+    hpt = hypertune.HyperTune()
+    hpt.report_hyperparameter_tuning_metric(
+        hyperparameter_metric_tag='rmse',
+        metric_value=hp_metric,
+        global_step=args['num_epochs'])
+
+    print("Exported trained model to {}".format(EXPORT_PATH))
--- blogs/babyweight/babyweight/trainer/task.py
+++ blogs/babyweight/babyweight/trainer/task.py
@@ -12,68 +12,98 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Example implementation of code to run on the Cloud ML service.
-"""
-
 import argparse
 import json
 import os
 
-import model
+from trainer import model
 
 import tensorflow as tf
-from tensorflow.contrib.learn.python.learn import learn_runner
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--bucket',
-      help='GCS path to data. We assume that data is in gs://BUCKET/babyweight/preproc/',
-      required=True
-  )
-  parser.add_argument(
-      '--output_dir',
-      help='GCS location to write checkpoints and export models',
-      required=True
-  )
-  parser.add_argument(
-      '--train_steps',
-      help='Steps to run the training job for.',
-      type=int,
-      default=10000
-  )
-  parser.add_argument(
-      '--pattern',
-      help='Specify a pattern that has to be in input files. For example 00001-of will process only one shard',
-      default='of'
-  )
-  parser.add_argument(
-      '--job-dir',
-      help='this model ignores this field, but it is required by gcloud',
-      default='junk'
-  )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--job-dir",
+        help="this model ignores this field, but it is required by gcloud",
+        default="junk"
+    )
+    parser.add_argument(
+        "--train_data_path",
+        help="GCS location of training data",
+        required=True
+    )
+    parser.add_argument(
+        "--eval_data_path",
+        help="GCS location of evaluation data",
+        required=True
+    )
+    parser.add_argument(
+        "--output_dir",
+        help="GCS location to write checkpoints and export models",
+        required=True
+    )
+    parser.add_argument(
+        "--batch_size",
+        help="Number of examples to compute gradient over.",
+        type=int,
+        default=512
+    )
+    parser.add_argument(
+        "--nnsize",
+        help="Hidden layer sizes for DNN -- provide space-separated layers",
+        nargs="+",
+        type=int,
+        default=[128, 32, 4]
+    )
+    parser.add_argument(
+        "--nembeds",
+        help="Embedding size of a cross of n key real-valued parameters",
+        type=int,
+        default=3
+    )
+    parser.add_argument(
+        "--num_epochs",
+        help="Number of epochs to train the model.",
+        type=int,
+        default=10
+    )
+    parser.add_argument(
+        "--train_examples",
+        help="""Number of examples (in thousands) to run the training job over.
+        If this is more than actual # of examples available, it cycles through
+        them. So specifying 1000 here when you have only 100k examples makes
+        this 10 epochs.""",
+        type=int,
+        default=5000
+    )
+    parser.add_argument(
+        "--eval_steps",
+        help="""Positive number of steps for which to evaluate model. Default
+        to None, which means to evaluate until input_fn raises an end-of-input
+        exception""",
+        type=int,
+        default=None
+    )
 
-  args = parser.parse_args()
-  arguments = args.__dict__
-  
-  # unused args provided by service
-  arguments.pop('job_dir', None)
-  arguments.pop('job-dir', None)
+    # Parse all arguments
+    args = parser.parse_args()
+    arguments = args.__dict__
 
-  output_dir = arguments.pop('output_dir')
-  model.BUCKET     = arguments.pop('bucket')
-  model.TRAIN_STEPS = arguments.pop('train_steps')
-  model.PATTERN = arguments.pop('pattern')
+    # Unused args provided by service
+    arguments.pop("job_dir", None)
+    arguments.pop("job-dir", None)
 
-  # Append trial_id to path if we are doing hptuning
-  # This code can be removed if you are not using hyperparameter tuning
-  output_dir = os.path.join(
-      output_dir,
-      json.loads(
-          os.environ.get('TF_CONFIG', '{}')
-      ).get('task', {}).get('trial', '')
-  )
+    # Modify some arguments
+    arguments["train_examples"] *= 1000
 
-  # Run the training job
-  #learn_runner.run(model.experiment_fn, output_dir)
-  model.train_and_evaluate(output_dir)
+    # Append trial_id to path if we are doing hptuning
+    # This code can be removed if you are not using hyperparameter tuning
+    arguments["output_dir"] = os.path.join(
+        arguments["output_dir"],
+        json.loads(
+            os.environ.get("TF_CONFIG", "{}")
+        ).get("task", {}).get("trial", "")
+    )
+
+    # Run the training job
+    model.train_and_evaluate(arguments)
