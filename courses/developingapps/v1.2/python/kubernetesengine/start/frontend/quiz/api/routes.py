60a0c07f61d23850be8bd11f37e95f70b4244ed9
Paul Leroy <32666273+paulleroyza@users.noreply.github.com>
2020-07-30 10:34:43 +0100

    Fixed import to fix kubernetes lab
    The PubSub code was updated but the kubernetes lab breaks as the import is still pointing at the old import.
    PubSub Lab Working (https://github.com/GoogleCloudPlatform/training-data-analyst/blob/master/courses/developingapps/v1.2/python/pubsub-languageapi-spanner/start/quiz/api/routes.py#L15)

--- courses/developingapps/v1.2/python/kubernetesengine/start/frontend/quiz/api/routes.py
+++ courses/developingapps/v1.2/python/kubernetesengine/start/frontend/quiz/api/routes.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import api
+from quiz.api import api
 
 from flask import request, Blueprint
 
