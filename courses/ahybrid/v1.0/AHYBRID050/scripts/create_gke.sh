1a526f309b1e0cafe308bc5d1269e75648b696c3
Jeff Davis <jwdavis@users.noreply.github.com>
2020-08-28 17:10:00 -0700

    hotfix-initial-commit
    - change gke setup script to use newest instructions

--- courses/ahybrid/v1.0/AHYBRID050/scripts/create_gke.sh
+++ courses/ahybrid/v1.0/AHYBRID050/scripts/create_gke.sh
@@ -55,24 +55,27 @@ curl --request POST \
   https://meshconfig.googleapis.com/v1alpha1/projects/${PROJECT_ID}:initialize
 
 # download anthos service mesh software
-curl -LO https://storage.googleapis.com/gke-release/asm/istio-1.6.4-asm.9-linux-amd64.tar.gz
-tar xzf istio-1.6.4-asm.9-linux-amd64.tar.gz
-cd istio-1.6.4-asm.9
+curl -LO https://storage.googleapis.com/gke-release/asm/istio-1.6.8-asm.9-linux-amd64.tar.gz
+tar xzf istio-1.6.8-asm.9-linux-amd64.tar.gz
+cd istio-1.6.8-asm.9
 export PATH=$PWD/bin:$PATH
 
-kpt pkg get https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages.git/asm@release-1.6-asm .
+kpt pkg get https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages.git/asm@release-1.6-asm asm
+kpt cfg set asm gcloud.core.project ${PROJECT_ID}
+kpt cfg set asm gcloud.project.environProjectNumber ${PROJECT_NUMBER}
 kpt cfg set asm gcloud.container.cluster ${C1_NAME}
 kpt cfg set asm gcloud.compute.location ${C1_ZONE}
+kpt cfg set asm anthos.servicemesh.profile asm-gcp
 
-istioctl install -f asm/cluster/istio-operator.yaml \
-  --set values.tracing.enabled=true \
-  --set values.global.proxy.tracer="stackdriver"
+istioctl install \
+  -f asm/cluster/istio-operator.yaml
+
+kubectl apply -f asm/canonical-service/controller.yaml
 
 kubectl wait --for=condition=available --timeout=600s deployment \
   --all -n istio-system
 
 kubectl label namespace default istio-injection=enabled --overwrite
-
 
 # Deploy BookInfo application
 kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
