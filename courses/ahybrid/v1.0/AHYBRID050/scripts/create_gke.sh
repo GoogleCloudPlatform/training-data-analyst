4f714383a3668d68dca0d79c5b99d9059d3b9b2e
Jeff Davis <jwdavis@users.noreply.github.com>
2020-08-29 10:28:32 -0700

    045-install
    Trying a version of 45 install; trying to get telemetry to work

--- courses/ahybrid/v1.0/AHYBRID050/scripts/create_gke.sh
+++ courses/ahybrid/v1.0/AHYBRID050/scripts/create_gke.sh
@@ -16,57 +16,56 @@
 
 source ./scripts/env.sh
 
-# create the gke cluster
 gcloud config set compute/zone ${C1_ZONE}
 gcloud beta container clusters create ${C1_NAME} \
     --machine-type=n1-standard-4 \
-    --num-nodes=4 \
+    --num-nodes=2 \
     --workload-pool=${WORKLOAD_POOL} \
     --enable-stackdriver-kubernetes \
     --subnetwork=default \
-    --labels=mesh_id=${MESH_ID} \
+    --labels mesh_id=${MESH_ID}
     --release-channel=regular
 
 # service account requires additional role bindings
 kubectl create clusterrolebinding [BINDING_NAME] \
     --clusterrole cluster-admin --user [USER]
 
-# create the service account for gke-connect
 gcloud iam service-accounts create connect-sa
 
-# assign GSA the role it needs
+echo ${PROJECT_ID}
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
  --member="serviceAccount:connect-sa@${PROJECT_ID}.iam.gserviceaccount.com" \
  --role="roles/gkehub.connect"
 
-# download the service account key
 gcloud iam service-accounts keys create connect-sa-key.json \
   --iam-account=connect-sa@${PROJECT_ID}.iam.gserviceaccount.com
 
-# register the cluster
 gcloud container hub memberships register ${C1_NAME}-connect \
    --gke-cluster=${C1_ZONE}/${C1_NAME}  \
    --service-account-key-file=./connect-sa-key.json
 
-# config project for Anthos Service Mesh
 curl --request POST \
   --header "Authorization: Bearer $(gcloud auth print-access-token)" \
   --data '' \
   https://meshconfig.googleapis.com/v1alpha1/projects/${PROJECT_ID}:initialize
 
-# download anthos service mesh software
+
 curl -LO https://storage.googleapis.com/gke-release/asm/istio-1.6.5-asm.7-linux-amd64.tar.gz
 tar xzf istio-1.6.5-asm.7-linux-amd64.tar.gz
 cd istio-1.6.5-asm.7
 export PATH=$PWD/bin:$PATH
 
 kpt pkg get https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages.git/. asm
+
 cd asm
 kpt cfg set asm gcloud.container.cluster ${C1_NAME}
 kpt cfg set asm gcloud.core.project ${PROJECT_ID}
 kpt cfg set asm gcloud.compute.location ${C1_ZONE}
+echo ${PROJECT_ID}
+echo ${C1_NAME}
+echo ${C1_ZONE}
 
-istioctl install -f asm/istio/istio-operator.yaml -f tracing.yaml
+istioctl install -f asm/istio/istio-operator.yaml -f asm/istio/tracing.yaml
 
 kubectl wait --for=condition=available --timeout=600s deployment \
   --all -n istio-system
