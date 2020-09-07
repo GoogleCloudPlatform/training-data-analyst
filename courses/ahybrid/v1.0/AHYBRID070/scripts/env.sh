2cfb0e3864608862467d4a6f60d39591ceecfbab
Hugo <hudomju@gmail.com>
2020-09-07 08:58:15 +0200

    AHYBRID070: Setup working directory

--- courses/ahybrid/v1.0/AHYBRID070/scripts/env.sh
+++ courses/ahybrid/v1.0/AHYBRID070/scripts/env.sh
@@ -1,26 +1,25 @@
-# Base dirs
-    export BASE_DIR=$(pwd)
-    export LAB_DIR=$BASE_DIR/workdir
-    export PATH=$PATH:$LAB_DIR/bin:
+# general values
+export PATH=$PATH:$LAB_DIR/bin:
+export HOME=~
 
 # GCP project
-    export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_ID=$(gcloud config get-value project)
 
 # Tools versions
-    export KUBECTX_VERSION=v0.7.0
-    export KOPS_VERSION=1.15.0
+export KUBECTX_VERSION=v0.7.0
+export KOPS_VERSION=1.15.0
 
 ## Setting variables for GKE
-    export CLUSTER_NAME="central"
-    export CLUSTER_ZONE="us-central1-b"
-    export CLUSTER_KUBECONFIG=$LAB_DIR/$CLUSTER_NAME.context
-    export WORKLOAD_POOL=${PROJECT_ID}.svc.id.goog
+export CLUSTER_NAME="central"
+export CLUSTER_ZONE="us-central1-b"
+export CLUSTER_KUBECONFIG=$LAB_DIR/$CLUSTER_NAME.context
+export WORKLOAD_POOL=${PROJECT_ID}.svc.id.goog
 
 # Variables for remote kops cluster
-    export REMOTE_CLUSTER_NAME_BASE="remote"
-    export REMOTE_CLUSTER_NAME=$REMOTE_CLUSTER_NAME_BASE.k8s.local
-    export KOPS_STORE=gs://$PROJECT_ID-kops-$REMOTE_CLUSTER_NAME_BASE
-    export REMOTE_KUBECONFIG=$LAB_DIR/remote.context
-    export NODE_COUNT=3
-    export NODE_SIZE=n1-standard-4
-    export ZONES=us-central1-a
+export REMOTE_CLUSTER_NAME_BASE="remote"
+export REMOTE_CLUSTER_NAME=$REMOTE_CLUSTER_NAME_BASE.k8s.local
+export KOPS_STORE=gs://$PROJECT_ID-kops-$REMOTE_CLUSTER_NAME_BASE
+export REMOTE_KUBECONFIG=$LAB_DIR/remote.context
+export NODE_COUNT=3
+export NODE_SIZE=n1-standard-4
+export ZONES=us-central1-a
