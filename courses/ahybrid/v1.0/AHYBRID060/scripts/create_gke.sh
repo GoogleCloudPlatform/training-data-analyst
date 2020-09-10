cb9aace02904cbfd25c8eab79af47025a0e879fb
Jeff Davis <jwdavis@users.noreply.github.com>
2020-09-10 12:52:20 -0700

    NEW-LAB060-UPGRADE-1.6.8

--- courses/ahybrid/v1.0/AHYBRID060/scripts/create_gke.sh
+++ courses/ahybrid/v1.0/AHYBRID060/scripts/create_gke.sh
@@ -48,23 +48,27 @@ curl --request POST \
   --data '' \
   https://meshconfig.googleapis.com/v1alpha1/projects/${PROJECT_ID}:initialize
 
-
-curl -LO https://storage.googleapis.com/gke-release/asm/istio-1.6.5-asm.7-linux-amd64.tar.gz
-tar xzf istio-1.6.5-asm.7-linux-amd64.tar.gz
-cd istio-1.6.5-asm.7
+# download anthos service mesh software
+curl -LO https://storage.googleapis.com/gke-release/asm/istio-1.6.8-asm.9-linux-amd64.tar.gz
+tar xzf istio-1.6.8-asm.9-linux-amd64.tar.gz
+cd istio-1.6.8-asm.9
 export PATH=$PWD/bin:$PATH
 
-kpt pkg get https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages.git/. asm
+kpt pkg get https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages@1.6.8-asm.9 asm
 
 cd asm
 kpt cfg set asm gcloud.container.cluster ${C1_NAME}
+kpt cfg set asm gcloud.project.environProjectNumber ${PROJECT_NUMBER}
 kpt cfg set asm gcloud.core.project ${PROJECT_ID}
 kpt cfg set asm gcloud.compute.location ${C1_ZONE}
+
+# To configure that all clusters are in the same project
+kpt cfg set asm anthos.servicemesh.profile asm-gcp
 
 gcloud container clusters get-credentials $C1_NAME \
     --zone $C1_ZONE --project $PROJECT_ID
 
-istioctl install -f asm/istio/istio-operator.yaml
+istioctl install -f asm/cluster/istio-operator.yaml
 
 kubectl wait --for=condition=available --timeout=600s deployment \
   --all -n istio-system
