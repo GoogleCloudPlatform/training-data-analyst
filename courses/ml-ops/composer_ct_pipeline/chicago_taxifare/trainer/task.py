b24314d779296a67b3e27237c826781788cfc3a9
Michael Abel <maabel0712@gmail.com>
2020-10-19 15:13:31 -0400

    Pushing new version of Composer CT demo in mlops

---
+++ courses/ml-ops/composer_ct_pipeline/chicago_taxifare/trainer/task.py
@@ -0,0 +1,47 @@
+import argparse
+
+from . import model
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--train_data_path",
+        help="GCS or local path to training data",
+        required=True
+    )
+    parser.add_argument(
+        "--train_epochs",
+        help="Steps to run the training job for (default: 5)",
+        type=int,
+        default=10
+    )
+    parser.add_argument(
+        "--eval_data_path",
+        help="GCS or local path to evaluation data",
+        required=True
+    )
+    parser.add_argument(
+        "--output_dir",
+        help="GCS location to write checkpoints and export models",
+        required=True
+    )
+    parser.add_argument(
+        "--output_ds",
+        help="BigQuery dataset where training metrics will be written",
+        required=True
+    )
+    parser.add_argument(
+        "--log_dir",
+        help="GCS location to write Tensorboard logs",
+        required=True
+    )
+    parser.add_argument(
+        "--job-dir",
+        help="This is not used by our model, but it is required by gcloud",
+    )
+    hparams = parser.parse_args().__dict__
+
+    model.train_and_evaluate(hparams)
