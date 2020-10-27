1ac67619ef45511f0ffa95059c073be8f9952202
Drew Hodun <dhodun@google.com>
2020-10-27 12:33:17 -0400

    container and image changes

--- quests/dataflow/gce_image/build.sh
+++ quests/dataflow/gce_image/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+IMAGE_NAME=theia-ide-training-data-analyst-v$(date +%Y%m%d)
+
 gcloud builds submit . --config cloudbuild.yaml --project qwiklabs-resources \
   --timeout=15m \
-  --substitutions=_TIMESTAMP=$(date +%Y%m%d)
+  --substitutions=_IMAGE_NAME=$IMAGE_NAME
