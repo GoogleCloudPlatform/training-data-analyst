c7660879497358f9f7260ff37d2c0b8266646c2e
Jeff Davis <jwdavis@users.noreply.github.com>
2020-11-17 11:22:25 -0800

    AHYBRID-045-COMMON-SCRIPTS
    - Change lab 045 setup to use common scripts directory

--- courses/ahybrid/v1.0/common/scripts/create_gke.sh
+++ courses/ahybrid/v1.0/common/scripts/create_gke.sh
@@ -14,37 +14,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-source ./scripts/env.sh
-
-gcloud config set compute/zone ${C1_ZONE}
 gcloud beta container clusters create ${C1_NAME} \
+    --zone ${C1_ZONE} \
+    --no-enable-basic-auth \
+    --enable-autoupgrade \
+    --max-surge-upgrade 1 \
+    --max-unavailable-upgrade 0 \
+    --enable-autorepair \
+    --image-type "COS" \
+    --release-channel "regular" \
     --machine-type=n1-standard-4 \
-    --num-nodes=2 \
+    --num-nodes=${C1_NODES} \
     --workload-pool=${WORKLOAD_POOL} \
     --enable-stackdriver-kubernetes \
-    --subnetwork=default \
+    --metadata disable-legacy-endpoints=true \
     --labels mesh_id=${MESH_ID} \
-    --release-channel=regular
+    --addons HorizontalPodAutoscaling,HttpLoadBalancing \
+    --default-max-pods-per-node "110" \
+    --enable-ip-alias \
+    --no-enable-master-authorized-networks \
+    --subnetwork=default \
+    --scopes ${C1_SCOPE} \
+    --tags=${C1_TAG}
+
 
 # service account requires additional role bindings
 kubectl create clusterrolebinding [BINDING_NAME] \
     --clusterrole cluster-admin --user [USER]
 
-gcloud iam service-accounts create connect-sa
+gcloud iam service-accounts create ${C1_NAME}-connect-sa
 
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
- --member="serviceAccount:connect-sa@${PROJECT_ID}.iam.gserviceaccount.com" \
+ --member="serviceAccount:${C1_NAME}-connect-sa@${PROJECT_ID}.iam.gserviceaccount.com" \
  --role="roles/gkehub.connect"
 
-gcloud iam service-accounts keys create connect-sa-key.json \
-  --iam-account=connect-sa@${PROJECT_ID}.iam.gserviceaccount.com
+gcloud iam service-accounts keys create ${C1_NAME}-connect-sa-key.json \
+  --iam-account=${C1_NAME}-connect-sa@${PROJECT_ID}.iam.gserviceaccount.com
 
 gcloud container hub memberships register ${C1_NAME}-connect \
    --gke-cluster=${C1_ZONE}/${C1_NAME}  \
-   --service-account-key-file=./connect-sa-key.json
-
-curl --request POST \
-  --header "Authorization: Bearer $(gcloud auth print-access-token)" \
-  --data '' \
-  https://meshconfig.googleapis.com/v1alpha1/projects/${PROJECT_ID}:initialize
-
+   --service-account-key-file=./${C1_NAME}-connect-sa-key.json
--- courses/ahybrid/v1.0/common/scripts/create_remote.sh
+++ courses/ahybrid/v1.0/common/scripts/create_remote.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-source ./scripts/env.sh
-
 gsutil mb $KOPS_STORE
 
 n=0
