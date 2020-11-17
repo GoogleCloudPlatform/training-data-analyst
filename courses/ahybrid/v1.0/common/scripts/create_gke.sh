8ebdd32e502f974f3b87d031f3c6adbc5d64d55f
Jeff Davis <jwdavis@users.noreply.github.com>
2020-11-17 11:37:48 -0800

    AHYBRID-045-TICKLE
    -- removed line to force git whisperer

--- courses/ahybrid/v1.0/common/scripts/create_gke.sh
+++ courses/ahybrid/v1.0/common/scripts/create_gke.sh
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 gcloud beta container clusters create ${C1_NAME} \
     --zone ${C1_ZONE} \
     --no-enable-basic-auth \
