e021ec579112d257ac3798eea6d78472f7ab4e6e
Jeff Davis <jwdavis@users.noreply.github.com>
2020-11-17 17:00:51 -0800

    AHYBRID-050-COMMON-T3

--- courses/ahybrid/v1.0/common/scripts/bookinfo.sh
+++ courses/ahybrid/v1.0/common/scripts/bookinfo.sh
@@ -1,10 +1,12 @@
 kubectl label namespace default istio-injection=enabled --overwrite
 
 # Deploy BookInfo application
-kubectl apply -f ../samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl apply -f \
+./istio-1.6.8-asm.9/samples/bookinfo/platform/kube/bookinfo.yaml
 
 # Sleep while Bookinfo pods initialize
 sleep 30s
 
 # Expose Bookinfo external gateway/IP
-kubectl apply -f ../samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl apply -f \
+.istio-1.6.8-asm.9/samples/bookinfo/networking/bookinfo-gateway.yaml
