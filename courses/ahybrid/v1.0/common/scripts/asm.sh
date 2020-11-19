029817c6ed49ea28d1c08bbe8c8598dc3c575727
Jeff Davis <jwdavis@users.noreply.github.com>
2020-11-19 15:18:29 -0800

    AHYBRID-050-173-T1
    -- working on 173 install

--- courses/ahybrid/v1.0/common/scripts/asm.sh
+++ courses/ahybrid/v1.0/common/scripts/asm.sh
@@ -1,34 +1,10 @@
-curl --request POST \
-  --header "Authorization: Bearer $(gcloud auth print-access-token)" \
-  --data '' \
-  https://meshconfig.googleapis.com/v1alpha1/projects/${PROJECT_ID}:initialize
+curl https://storage.googleapis.com/csm-artifacts/asm/install_asm_1.7 > install_asm
 
-
-curl -LO https://storage.googleapis.com/gke-release/asm/istio-1.6.8-asm.9-linux-amd64.tar.gz
-tar xzf istio-1.6.8-asm.9-linux-amd64.tar.gz
-cd istio-1.6.8-asm.9
-export PATH=$PWD/bin:$PATH
-
-kpt pkg get https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages@1.6.8-asm.9 asm
-
-cd asm
-kpt cfg set asm gcloud.container.cluster ${C1_NAME}
-kpt cfg set asm gcloud.project.environProjectNumber ${PROJECT_NUMBER}
-kpt cfg set asm gcloud.core.project ${PROJECT_ID}
-kpt cfg set asm gcloud.compute.location ${C1_ZONE}
-
-# To configure that all clusters are in the same project
-kpt cfg set asm anthos.servicemesh.profile asm-gcp
-
-gcloud container clusters get-credentials $C1_NAME \
-    --zone $C1_ZONE --project $PROJECT_ID
-
-# Install Istio + Enable tracing with Cloud Trace
-istioctl install -f asm/cluster/istio-operator.yaml -f $LAB_DIR/training-data-analyst/courses/ahybrid/v1.0/AHYBRID050/scripts/tracing.yaml
-
-# Enable the Anthos Service Mesh UI in Cloud Console
-kubectl apply -f asm/canonical-service/controller.yaml
-
-kubectl wait --for=condition=available --timeout=600s deployment \
-  --all -n istio-system
-
+chmod +x install_asm
+./install_asm \
+--project_id $PROJECT_ID \
+--cluster_name $CLUSTER_NAME \
+--cluster_location $CLUSTER_ZONE \
+--mode install \
+--enable_apis \
+--customer_overlay $LAB_DIR/training-data-analyst/courses/ahybrid/v1.0/AHYBRID050/scripts/tracing.yaml
--- courses/ahybrid/v1.0/common/scripts/bookinfo.sh
+++ courses/ahybrid/v1.0/common/scripts/bookinfo.sh
@@ -1,12 +1,12 @@
+kubectl -n istio-system get pods -l app=istiod --show-labels
+
 kubectl label namespace default istio-injection=enabled --overwrite
 
 # Deploy BookInfo application
-kubectl apply -f \
-./istio-1.6.8-asm.9/samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/platform/kube/bookinfo.yaml
 
 # Sleep while Bookinfo pods initialize
 sleep 30s
 
 # Expose Bookinfo external gateway/IP
-kubectl apply -f \
-./istio-1.6.8-asm.9/samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/networking/bookinfo-gateway.yaml
