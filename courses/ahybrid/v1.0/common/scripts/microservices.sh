ce833ca02d7686ed71ee1184bcac3bd09a4df42c
Hugo Doménech Juárez <hugo@Hugos-MacBook-Pro.local>
2020-12-22 13:03:22 +0100

    AHYBRID: add microservices demo installation script

---
+++ courses/ahybrid/v1.0/common/scripts/microservices.sh
@@ -0,0 +1,7 @@
+kubectl create namespace prod
+kubectl label namespace prod \
+  istio.io/rev=$(kubectl -n istio-system get pods -l app=istiod -o json | jq -r '.items[0].metadata.labels["istio.io/rev"]') \
+  --overwrite
+kubectl apply -n prod -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml
+kubectl apply -n prod -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/istio-manifests.yaml
+kubectl patch -n prod deployments/productcatalogservice -p '{"spec":{"template":{"metadata":{"labels":{"version":"v1"}}}}}'
