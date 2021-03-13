dee30a727e2a5f44d752433704e19ce539e76033
Michael Abel <maabel0712@gmail.com>
2021-03-12 20:33:56 -0500

    Added code for Python versions of Dataflow labs

---
+++ quests/dataflow_python/create_batch_sinks.sh
@@ -0,0 +1,12 @@
+#!/bin/#!/usr/bin/env bash
+echo "Creating pipeline sinks"
+
+PROJECT_ID=$(gcloud config get-value project)
+
+# GCS buckets
+#TODO: Add try/catch for the first bucket since qwiklabs
+gsutil mb -l US gs://$PROJECT_ID
+gsutil mb -l US -c "COLDLINE" gs://$PROJECT_ID-coldline
+
+# BiqQuery Dataset
+bq mk --location=US logs
---
+++ quests/dataflow_python/install_packages.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+pip3 install -q anytree
+pip3 install -q --upgrade google-cloud-pubsub
+pip3 install -q faker
+pip3 install -q --upgrade geocoder
