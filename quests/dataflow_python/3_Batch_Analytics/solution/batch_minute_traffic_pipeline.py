4b402730d18b1b8e4c0ab7b8e59652906e57e74f
Michael Abel <maabel0712@gmail.com>
2021-03-14 14:33:46 -0400

    Fixed bug with DoFn in batch_minute_traffic_pipeline

--- quests/dataflow_python/3_Batch_Analytics/solution/batch_minute_traffic_pipeline.py
+++ quests/dataflow_python/3_Batch_Analytics/solution/batch_minute_traffic_pipeline.py
@@ -38,7 +38,7 @@ class GetTimestampFn(beam.DoFn):
     def process(self, element, window=beam.DoFn.WindowParam):
         window_start = window.start.to_utc_datetime().strftime("%Y-%m-%dT%H:%M:%S")
         output = {'page_views': element, 'timestamp': window_start}
-        return output
+        yield output
 
 # ### main
 
