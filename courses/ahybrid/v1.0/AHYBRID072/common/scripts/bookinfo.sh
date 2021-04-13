cfdf4e0f1e87993916702f9168e4b796b9eb4071
Jeff Davis <jwdavis@users.noreply.github.com>
2021-04-13 15:25:37 -0700

    anthos-072-addconfig
    - add config files
    - remove common files from 072

--- courses/ahybrid/v1.0/AHYBRID072/common/scripts/bookinfo.sh
+++
@@ -1,8 +0,0 @@
-# Deploy BookInfo application
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/platform/kube/bookinfo.yaml
-
-# Sleep while Bookinfo pods initialize
-sleep 30s
-
-# Expose Bookinfo external gateway/IP
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/networking/bookinfo-gateway.yaml
