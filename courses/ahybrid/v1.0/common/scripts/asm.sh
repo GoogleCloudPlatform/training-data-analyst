a81940f5068fa96e27016af381a8f8b82832069d
Hugo Domenech <hugo@yourcloudtrainer.com>
2021-05-02 18:01:13 +0200

    ahybrid-commons: upgrade asm 1.7 to asm 1.9

--- courses/ahybrid/v1.0/common/scripts/asm.sh
+++ courses/ahybrid/v1.0/common/scripts/asm.sh
@@ -1,7 +1,7 @@
-# curl https://storage.googleapis.com/csm-artifacts/asm/install_asm_1.7 > install_asm
+# curl https://storage.googleapis.com/csm-artifacts/asm/install_asm_1.9 > install_asm
 
-gcloud iam service-accounts keys create qwiklabs-user-sa.json \
-  --iam-account=${PROJECT_ID}@${PROJECT_ID}.iam.gserviceaccount.com
+# gcloud iam service-accounts keys create qwiklabs-user-sa.json \
+#   --iam-account=${PROJECT_ID}@${PROJECT_ID}.iam.gserviceaccount.com
 
 chmod +x ../common/scripts/install_asm
 
@@ -9,11 +9,21 @@ chmod +x ../common/scripts/install_asm
 --project_id $PROJECT_ID \
 --cluster_name $C1_NAME \
 --cluster_location $C1_ZONE \
---service_account ${PROJECT_ID}@${PROJECT_ID}.iam.gserviceaccount.com \
---key_file qwiklabs-user-sa.json \
 --mode install \
---enable_apis \
+--enable_gcp_apis \
+--enable_all \
 --custom_overlay $LAB_DIR/training-data-analyst/courses/ahybrid/v1.0/common/scripts/tracing.yaml
+
+
+# ../common/scripts/install_asm \
+# --project_id $PROJECT_ID \
+# --cluster_name $C1_NAME \
+# --cluster_location $C1_ZONE \
+# --service_account ${PROJECT_ID}@${PROJECT_ID}.iam.gserviceaccount.com \
+# --key_file qwiklabs-user-sa.json \
+# --mode install \
+# --enable_apis \
+# --custom_overlay $LAB_DIR/training-data-analyst/courses/ahybrid/v1.0/common/scripts/tracing.yaml
 
 kubectl label namespace default \
   istio.io/rev=$(kubectl -n istio-system get pods -l app=istiod -o json | jq -r '.items[0].metadata.labels["istio.io/rev"]') \
