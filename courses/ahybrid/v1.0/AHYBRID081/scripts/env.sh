870c01913cc7b4d6eeb78922d832a29c6441ec68
Jeff Davis <jwdavis@users.noreply.github.com>
2021-06-22 13:36:58 -0700

    Finish Registration Retries
    added retry logic for registration failure
    changed zone for remote cluster creation

--- courses/ahybrid/v1.0/AHYBRID081/scripts/env.sh
+++ courses/ahybrid/v1.0/AHYBRID081/scripts/env.sh
@@ -15,7 +15,7 @@ export MESH_ID="proj-${PROJECT_NUMBER}"
 # Variables for remote kops cluster
 export C2_NAME="remote"
 export C2_FULLNAME=$C2_NAME.k8s.local
-export C2_ZONE="us-east1-b"
+export C2_ZONE="us-west1-b"
 export NODE_COUNT=2
 export NODE_SIZE=n1-standard-4
 export KOPS_ZONES=$C2_ZONE
