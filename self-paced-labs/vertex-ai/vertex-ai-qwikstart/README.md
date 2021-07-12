cca246d1546117abb52f6ecc55889757cbbe5e92
Doug Kelly <dkelly628@gmail.com>
2021-07-12 08:22:53 +0000

    README and lab text updates.

--- self-paced-labs/vertex-ai/vertex-ai-qwikstart/README.md
+++ self-paced-labs/vertex-ai/vertex-ai-qwikstart/README.md
@@ -1,18 +1,64 @@
 # Vertex AI: Qwikstart
 
-In this lab, you will walk through an introductory, end-to-end training and deployment workflow for a custom TensorFlow model on Vertex AI.
+In this lab, you will walk through an introductory, end-to-end training and deployment workflow for a custom container TensorFlow model on Vertex AI.
 
 ## Learning objectives
 
-* Train a TensorFlow model locally in a Vertex Notebook.
-* Create a managed dataset artifact for experiment tracking.
-* Containerize your training code and push it Google Container Registry.
-* Run a training job on Vertex AI with your custom model container.
-* Use Vertex TensorBoard to monitor model performance during training.
-* Deploy your trained model to a Vertex Prediction endpoint for serving predictions.
+* Train a TensorFlow model locally in a hosted [**Vertex Notebook**](https://cloud.google.com/vertex-ai/docs/general/notebooks?hl=sv).
+* Create a [**managed Tabular dataset**](https://cloud.google.com/vertex-ai/docs/training/using-managed-datasets?hl=sv) artifact for experiment tracking.
+* Containerize your training code with [**Cloud Build**](https://cloud.google.com/build) and push it to [**Google Cloud Container Registry**](https://cloud.google.com/container-registry).
+* Run a [**Vertex AI custom training job**]() with your custom model container.
+* Use [**Vertex TensorBoard**](https://cloud.google.com/vertex-ai/docs/experiments/tensorboard-overview) to visualize model performance.
+* Deploy your trained model to a [**Vertex Prediction Endpoint**](https://cloud.google.com/vertex-ai/docs/predictions/getting-predictions) for serving predictions.
 * Request an online prediction and see the response.
 
 ## Setup
 
-Enable APIs
+To enable Cloud Services utilized in the lab environment:
 
+1. Launch [Cloud Shell](https://cloud.google.com/shell/docs/launching-cloud-shell)
+2. Set your project ID
+```
+PROJECT_ID=[YOUR PROJECT ID]
+
+gcloud config set project $PROJECT_ID
+```
+3. Use `gcloud` to enable the services
+```
+gcloud services enable \
+compute.googleapis.com \
+iam.googleapis.com \
+cloudbuild.googleapis.com \
+container.googleapis.com \
+notebooks.googleapis.com \
+aiplatform.googleapis.com \
+dataflow.googleapis.com \
+bigquery.googleapis.com \
+bigquerydatatransfer.googleapis.com \  
+artifactregistry.googleapis.com \
+cloudresourcemanager.googleapis.com \
+cloudtrace.googleapis.com \
+iamcredentials.googleapis.com \
+monitoring.googleapis.com \
+logging.googleapis.com
+```
+
+## Creating a Vertex Notebooks instance
+
+An instance of **Vertex Notebooks** is used as a primary lab environment.
+
+To provision the instance follow the [Create an new notebook instance](https://cloud.google.com/vertex-ai/docs/general/notebooks) setup guide. Use the *TensorFlow Enterprise 2.3* no-GPU image. Leave all other settings at their default values.
+
+After the instance is created, you can connect to [JupyterLab](https://jupyter.org/) IDE by clicking the *OPEN JUPYTERLAB* link in the [Vertex AI Notebooks Console](https://console.cloud.google.com/vertex-ai/notebooks/instances).
+
+In the **JupyterLab**, open a terminal and clone this repository in the `home` folder.
+```
+cd
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst.git
+```
+
+Navigate to the lab folder and happy coding.
+
+```
+cd training-data-analyst/tree/master/self-paced-labs/vertex-ai/vertex-ai-qwikstart
+```
--- self-paced-labs/vertex-ai/vertex-ai-qwikstart/trainer/task.py
+++ self-paced-labs/vertex-ai/vertex-ai-qwikstart/trainer/task.py
@@ -1,8 +1,8 @@
-#
-import os
-import tensorflow as tf
 
-from tensorflow.python.framework import ops
+import os
+import tempfile
+import tensorflow as tf
+from explainable_ai_sdk.metadata.tf.v2 import SavedModelMetadataBuilder
 from tensorflow.python.framework import dtypes
 from tensorflow_io.bigquery import BigQueryClient
 from tensorflow_io.bigquery import BigQueryReadSession
@@ -12,10 +12,10 @@ from tensorflow_io.bigquery import BigQueryReadSession
 # https://medium.com/google-cloud/ml-design-pattern-3-virtual-epochs-f842296de730
 N_TRAIN_EXAMPLES = 2638
 N_DEV_EXAMPLES = 353
-STOP_POINT = 2.0
+STOP_POINT = 10.0
 TOTAL_TRAIN_EXAMPLES = int(STOP_POINT * N_TRAIN_EXAMPLES)
 BATCH_SIZE = 16
-N_CHECKPOINTS = 2
+N_CHECKPOINTS = 10
 STEPS_PER_EPOCH = (TOTAL_TRAIN_EXAMPLES // (BATCH_SIZE*N_CHECKPOINTS))
 
 
@@ -25,13 +25,11 @@ test_data_uri = os.environ["AIP_TEST_DATA_URI"]
 data_format = os.environ["AIP_DATA_FORMAT"]
 
 NUMERIC_FEATURES = [
-    "customer_id",
     "n_purchases",
     "avg_purchase_size",
     "avg_purchase_revenue",
     "customer_age",
     "days_since_last_purchase",
-    "target_monetary_value_3M",
 ]
 
 CATEGORICAL_FEATURES = [
@@ -61,15 +59,14 @@ def read_bigquery(project, dataset, table):
       dataset_id=dataset,
       table_id=table,
       selected_fields=NUMERIC_FEATURES + [LABEL],
-      output_types=[dtypes.float64] * 7 + [dtypes.string],
+      output_types=[dtypes.int64, dtypes.float64, dtypes.float64, dtypes.int64, dtypes.int64] + [dtypes.float64],
       requested_streams=2)
     dataset = read_session.parallel_read_rows()
     transformed_ds = dataset.map(features_and_labels)
     return transformed_ds
 
-BATCH_SIZE = 16
 
-training_ds = read_bigquery(*caip_uri_to_fields(training_data_uri)).prefetch(1).shuffle(BATCH_SIZE*10).batch(BATCH_SIZE)
+training_ds = read_bigquery(*caip_uri_to_fields(training_data_uri)).prefetch(1).shuffle(BATCH_SIZE*10).batch(BATCH_SIZE).repeat()
 eval_ds = read_bigquery(*caip_uri_to_fields(validation_data_uri)).prefetch(1).shuffle(BATCH_SIZE*10).batch(BATCH_SIZE)
 test_ds = read_bigquery(*caip_uri_to_fields(test_data_uri)).prefetch(1).shuffle(BATCH_SIZE*10).batch(BATCH_SIZE)
 
@@ -80,7 +77,7 @@ feature_columns = {
 }
 
 
-# Create a custom evalution metric.
+# Create a custom RMSE evalution metric.
 def rmse(y_true, y_pred):
     return tf.sqrt(tf.reduce_mean(tf.square(y_pred - y_true)))
 
@@ -91,45 +88,54 @@ def build_model():
     model = tf.keras.Sequential([
         tf.keras.layers.DenseFeatures(feature_columns.values()),
         tf.keras.layers.Dense(64, activation=tf.nn.relu),
+        tf.keras.layers.Dropout(0.1),        
         tf.keras.layers.Dense(64, activation=tf.nn.relu),
         tf.keras.layers.Dense(1)
     ])
     
-    optimizer = tf.keras.optimizers.Adam(0.001)
-    
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(
-#         log_dir=os.environ['AIP_TENSORBOARD_LOG_DIR'],
-        log_dir=os.environ["AIP_MODEL_DIR"] + '/tensorboard-logs',
-        histogram_freq=1)
-    
-    earlystopping_callback = tf.keras.callbacks.EarlyStopping(patience=2)
-    
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=os.environ["AIP_MODEL_DIR"] + '/checkpoints',
-        save_weights_only=True,
-        monitor='val_loss',
-        mode='min')    
+    optimizer = tf.keras.optimizers.Adam(0.001)    
     
     model.compile(loss='mse',
                   optimizer=optimizer,
-                  callbacks=[[tensorboard_callback, 
-                              earlystopping_callback,
-                              checkpoint_callback]],
                   metrics=[['mae', 'mse', rmse]])
     
     return model
 
 model = build_model()
 
+tensorboard_callback = tf.keras.callbacks.TensorBoard(
+#         log_dir=os.environ['AIP_TENSORBOARD_LOG_DIR'],
+    log_dir=os.environ["AIP_MODEL_DIR"] + '/tensorboard-logs',
+    histogram_freq=1)
+
+earlystopping_callback = tf.keras.callbacks.EarlyStopping(patience=2)
+
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=os.environ["AIP_MODEL_DIR"] + '/checkpoints',
+    save_weights_only=True,
+    monitor='val_loss',
+    mode='min')
+
 model.fit(training_ds,
           validation_data=eval_ds,
           steps_per_epoch=STEPS_PER_EPOCH,
           epochs=N_CHECKPOINTS,
-          callbacks=[tensorboard_callback])
+          callbacks=[[tensorboard_callback,
+                      earlystopping_callback,
+                      checkpoint_callback]])
 
 print(model.evaluate(test_ds))
 
 # rankdir='LR' is used to make the graph horizontal.
 # tf.keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
 
-tf.saved_model.save(model, os.environ["AIP_MODEL_DIR"])
+tmpdir = tempfile.mkdtemp()
+
+# Export Keras model in TensorFlow SavedModel format.
+model.save(tmpdir)
+
+# Annotate and save TensorFlow SavedModel with Explainable metadata to GCS.
+builder = SavedModelMetadataBuilder(tmpdir)
+builder.save_model_with_metadata(os.environ["AIP_MODEL_DIR"])
+
+# tf.saved_model.save(model, os.environ["AIP_MODEL_DIR"])
