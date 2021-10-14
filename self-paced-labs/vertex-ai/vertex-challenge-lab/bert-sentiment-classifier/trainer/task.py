f32c1c6a2baa317ddc3cb177c73d441679dafc25
Doug Kelly <dkelly628@gmail.com>
2021-10-14 23:56:37 +0000

    Add fix to b/203105209 to solution notebook.

--- self-paced-labs/vertex-ai/vertex-challenge-lab/bert-sentiment-classifier/trainer/task.py
+++ self-paced-labs/vertex-ai/vertex-challenge-lab/bert-sentiment-classifier/trainer/task.py
@@ -6,14 +6,13 @@ from trainer import model
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
     # Vertex custom container training args. These are set by Vertex AI during training but can also be overwritten.
     parser.add_argument('--model-dir', dest='model-dir',
-                        default=os.environ['AIP_MODEL_DIR'], type=str, help='GCS URI for saving model artifacts.')
-    # parser.add_argument('--data-dir', dest='data-dir',
-    #                     default='gs://dougkelly-vertex-demos-vertex-challenge-lab/data/acllmdb', type=str, help='GCS URI for saving dataset.') 
-    parser.add_argument('--tf-hub-bert-preprocessor', dest='tf-hub-bert-preprocessor', 
+                        default=os.environ['AIP_MODEL_DIR'], type=str, help='GCS URI for saving model artifacts.') 
+    parser.add_argument('--tfhub-bert-preprocessor', dest='tfhub-bert-preprocessor', 
                         default='https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3', type=str, help='TF-Hub URL.')
-    parser.add_argument('--tf-hub-bert-encoder', dest='tf-hub-bert-encoder', 
+    parser.add_argument('--tfhub-bert-encoder', dest='tfhub-bert-encoder', 
                         default='https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/2', type=str, help='TF-Hub URL.')     
 
     # Model training args.
@@ -21,7 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', dest='epochs', default=5, type=int, help='Training iterations.')    
     parser.add_argument('--batch-size', dest='batch-size', default=32, type=int, help='Number of examples during each training iteration.')    
     parser.add_argument('--dropout', dest='dropout', default=0.1, type=float, help='Float percentage of DNN nodes [0,1] to drop for regularization.')    
-
+    parser.add_argument('--seed', dest='seed', default=42, type=int, help='Random number generator seed to prevent overlap between train and val sets.')
     
     args = parser.parse_args()
     hparams = args.__dict__
