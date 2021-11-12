34dbb3a6375374250299a01908edd26eaf187155
Mike Dunker <mdunker@google.com>
2021-11-12 12:55:15 -0800

    make echos more prominent

--- quests/develop-apis-apigee/grpc-backend/init-project.sh
+++ quests/develop-apis-apigee/grpc-backend/init-project.sh
@@ -3,18 +3,18 @@ MYDIR="$(dirname "$0")"
 source "${MYDIR}/config.sh"
 
 if [[ -z "${APP_REGION}" ]]; then
-  echo "APP_REGION not set"
+  echo "*** APP_REGION not set ***"
   exit 1
 fi
 
 # enable Cloud Run APIs
-echo "enable Cloud Run APIs"
+echo "*** enable Cloud Run APIs ***"
 gcloud services enable run.googleapis.com
 
 # create App Engine app (prerequisite for Firestore in Native mode)
-echo "create App Engine app (required for Firestore)"
+echo "*** create App Engine app (required for Firestore) ***"
 gcloud app create --region=${APP_REGION}
 
 # create Firestore in Native mode database
-echo "create Firestore database"
+echo "*** create Firestore database ***"
 gcloud firestore databases create --region=${APP_REGION}
--- quests/develop-apis-apigee/rest-backend/apigee.sh
+++ quests/develop-apis-apigee/rest-backend/apigee.sh
@@ -3,12 +3,12 @@ MYDIR="$(dirname "$0")"
 source "${MYDIR}/config.sh"
 
 if [[ -z "${GOOGLE_PROJECT_ID}" ]]; then
-  echo "GOOGLE_PROJECT_ID not set"
+  echo "*** GOOGLE_PROJECT_ID not set ***"
   exit 1
 fi
 
 if [[ -z "${CLOUDRUN_REGION}" ]]; then
-  echo "CLOUDRUN_REGION not set"
+  echo "*** CLOUDRUN_REGION not set ***"
   exit 1
 fi
 
@@ -18,14 +18,14 @@ APIGEE_SVCACCT_EMAIL="${APIGEE_SVCACCT_NAME}@${GOOGLE_PROJECT_ID}.iam.gserviceac
 APIGEE_SVCACCT_ROLE="roles/run.invoker"
 
 # create service account for Apigee to access Cloud Run service
-echo "creating Apigee service account: ${APIGEE_SVCACCT_EMAIL}"
+echo "*** creating Apigee service account: ${APIGEE_SVCACCT_EMAIL} ***"
 gcloud iam service-accounts create ${APIGEE_SVCACCT_NAME} \
   --display-name="Service account for internal access by Apigee proxies" \
   --project=${GOOGLE_PROJECT_ID}
 
 # add permission to invoke REST service
-echo "adding role ${APIGEE_SVCACCT_ROLE} for service ${SERVICE_NAME}"
+echo "*** adding role ${APIGEE_SVCACCT_ROLE} for service ${SERVICE_NAME} ***"
 gcloud run services add-iam-policy-binding ${SERVICE_NAME} \
-  --member="serviceAccount:${APIGEE_SERVICE_ACCT}" \
+  --member="serviceAccount:${APIGEE_SVCACCT_EMAIL}" \
   --role=${APIGEE_SVCACCT_ROLE} --region=${CLOUDRUN_REGION} \
   --project=${GOOGLE_PROJECT_ID}
