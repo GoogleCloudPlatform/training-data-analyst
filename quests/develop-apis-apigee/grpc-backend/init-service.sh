fc52b580bf7900626a477cdcbda2c84914c74db2
Mike Dunker <mdunker@google.com>
2021-12-03 09:45:34 -0800

    updating develop & secure APIs quest backend (#1762)
    * make echos more prominent
    
    * clean up

--- quests/develop-apis-apigee/grpc-backend/init-service.sh
+++ quests/develop-apis-apigee/grpc-backend/init-service.sh
@@ -3,7 +3,7 @@ MYDIR="$(dirname "$0")"
 source "${MYDIR}/config.sh"
 
 if [[ -z "${GOOGLE_PROJECT_ID}" ]]; then
-  echo "GOOGLE_PROJECT_ID not set"
+  echo "*** GOOGLE_PROJECT_ID not set ***"
   exit 1
 fi
 
@@ -12,13 +12,13 @@ SVCACCT_EMAIL="${SVCACCT_NAME}@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com"
 SVCACCT_ROLE="roles/datastore.user"
 
 # create service account for Cloud Run service
-echo "creating Cloud Run service account: ${SVCACCT_EMAIL}"
+echo "*** creating Cloud Run service account: ${SVCACCT_EMAIL} ***"
 gcloud iam service-accounts create ${SVCACCT_NAME} \
   --display-name="Simplebank(gRPC) service account" \
   --project=${GOOGLE_PROJECT_ID}
 
 # add permission to access Firestore
-echo "adding role ${SVCACCT_ROLE} for Firestore access"
+echo "*** adding role ${SVCACCT_ROLE} for Firestore access ***"
 gcloud projects add-iam-policy-binding ${GOOGLE_PROJECT_ID} \
   --member="serviceAccount:${SVCACCT_EMAIL}" \
   --role=${SVCACCT_ROLE}
