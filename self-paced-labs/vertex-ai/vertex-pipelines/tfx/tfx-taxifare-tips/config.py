c84c1fd712459f180cd5955628ef91bf80d7eb0f
Doug Kelly <dkelly628@gmail.com>
2021-12-06 02:56:48 +0000

    Rename files.

--- self-paced-labs/vertex-ai/vertex-pipelines/tfx/tfx-taxifare-tips/config.py
+++
@@ -1,7 +0,0 @@
-"""TFX pipeline configurations."""
-
-import os
-
-GOOGLE_CLOUD_PROJECT = os.getenv("PROJECT", "")
-GOOGLE_CLOUD_REGION = os.getenv("REGION", "")
-GCS_LOCATION = os.getenv("GCS_LOCATION", "")
