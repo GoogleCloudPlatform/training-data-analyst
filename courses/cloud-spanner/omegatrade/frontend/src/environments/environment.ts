3029961499c3554c68ac54b9bbb5f0ae78fbac08
Damon A. Runion <48971473+damonarunion@users.noreply.github.com>
2022-05-26 12:27:27 -0500

    Update environment.ts

--- courses/cloud-spanner/omegatrade/frontend/src/environments/environment.ts
+++ courses/cloud-spanner/omegatrade/frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   name: "dev",
   // change baseUrl according to backend URL
-  baseUrl:"backend_URL_from_deploy/v1/",
+  baseUrl:"{Backend_URL_from_Deploy}/v1/",
   // change clientId to actual value you have received from Oauth console
   clientId:''
 };
