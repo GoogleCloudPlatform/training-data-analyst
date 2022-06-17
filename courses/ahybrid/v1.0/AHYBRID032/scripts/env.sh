51bf5557abdcdcf4ca7a8199485708808295a29a
Hugo Doménech Juárez <hudomju@gmail.com>
2022-06-17 16:51:09 +0200

    ahybrid032: change zone to not run out of resources

--- courses/ahybrid/v1.0/AHYBRID032/scripts/env.sh
+++ courses/ahybrid/v1.0/AHYBRID032/scripts/env.sh
@@ -5,7 +5,7 @@ export PROJECT_ID=$(gcloud config get-value project)
 
 # gke cluster values
 export C1_NAME="gke-west"
-export C1_ZONE="us-west1-a"
+export C1_ZONE="us-west2-a"
 export C1_NODES=2
 export C1_SCOPE="https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append"
 export PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} \
