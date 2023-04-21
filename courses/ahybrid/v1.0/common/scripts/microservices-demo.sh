7dbf424d3f1ba81cafe6d7476d6d03d2109257d5
Hugo Doménech Juárez <hudomju@gmail.com>
2023-04-21 13:28:21 +0200

    ahybrid: install missing istio gateway

--- courses/ahybrid/v1.0/common/scripts/microservices-demo.sh
+++ courses/ahybrid/v1.0/common/scripts/microservices-demo.sh
@@ -8,6 +8,13 @@ then
 else
   kubectl label namespace prod istio-injection=enabled --overwrite
 fi
+
+# Install the Istio Gateway
+git clone https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages
+kubectl apply -n prod -f anthos-service-mesh-packages/samples/gateways/istio-ingressgateway
+kubectl apply -n prod -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/istio-manifests.yaml
+
+# Install the App
 kubectl apply -n prod -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml
 kubectl apply -n prod -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/istio-manifests.yaml
 kubectl patch -n prod deployments/productcatalogservice -p '{"spec":{"template":{"metadata":{"labels":{"version":"v1"}}}}}'
