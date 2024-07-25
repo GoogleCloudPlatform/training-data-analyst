4695c9e8e93acff6bd8a625632825502f61a8651
Mike Dunker <mdunker@google.com>
2024-07-25 09:41:12 -0700

    turn off debug mode

--- courses/codeassist/main.py
+++ courses/codeassist/main.py
@@ -32,4 +32,4 @@ def convert():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
