f8334ac794b1ffe09ab314aed63007a06dddd597
anjalikumari-ssk <anjali.kumari@sureskills.com>
2024-08-19 17:51:57 +0530

    Updated the `prepare_environment.sh` file.

--- courses/developingapps/v1.3/nodejs/cloudfunctions/start/prepare_environment.sh
+++ courses/developingapps/v1.3/nodejs/cloudfunctions/start/prepare_environment.sh
@@ -15,7 +15,7 @@ echo "Enabling Cloud Functions API"
 gcloud services enable cloudfunctions.googleapis.com
 
 echo "Creating App Engine app"
-gcloud app create --region "us-east1"
+gcloud app create --region "us-central"
 
 echo "Making bucket: gs://$DEVSHELL_PROJECT_ID-media"
 gsutil mb gs://$DEVSHELL_PROJECT_ID-media
@@ -36,7 +36,7 @@ echo "Creating Cloud Pub/Sub topic"
 gcloud pubsub topics create feedback
 
 echo "Creating Cloud Spanner Instance, Database, and Table"
-gcloud spanner instances create quiz-instance --config=regional-us-east1 --description="Quiz instance" --nodes=1
+gcloud spanner instances create quiz-instance --config=regional-us-central1 --description="Quiz instance" --nodes=1
 gcloud spanner databases create quiz-database --instance quiz-instance --ddl "CREATE TABLE Feedback ( feedbackId STRING(100) NOT NULL, email STRING(100), quiz STRING(20), feedback STRING(MAX), rating INT64, score FLOAT64, timestamp INT64 ) PRIMARY KEY (feedbackId);"
 
 echo "Project ID: $DEVSHELL_PROJECT_ID"
