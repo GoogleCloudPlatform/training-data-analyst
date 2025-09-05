c323a322dedbbdc976c97b417ad58904f4b74837
Mike Dunker <mdunker@google.com>
2025-09-05 10:12:07 -0700

    modify to use artifact registry

--- quests/develop-apis-apigee/rest-backend/deploy.sh
+++ quests/develop-apis-apigee/rest-backend/deploy.sh
@@ -14,18 +14,22 @@ fi
 
 SERVICE_NAME="simplebank-rest"
 SVCACCT_NAME="simplebank-rest"
+REPOSITORY_NAME="simplebank-rest-repo"
 SVCACCT_EMAIL="${SVCACCT_NAME}@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com"
+
+# create artifact registry repo
+gcloud artifacts repositories create ${REPOSITORY_NAME} --repository-format=docker --location=${CLOUDRUN_REGION} --project=${GOOGLE_PROJECT_ID}
 
 # build image from code
 echo "*** submit build of service ${SERVICE_NAME} to Cloud Build ***"
-gcloud builds submit --tag gcr.io/${GOOGLE_PROJECT_ID}/${SERVICE_NAME} \
+gcloud builds submit --tag ${CLOUDRUN_REGION}-docker.pkg.dev/${GOOGLE_PROJECT_ID}/${REPOSITORY_NAME}/${SERVICE_NAME} \
   --project=${GOOGLE_PROJECT_ID}
 
 # deploy service
 # NOTE: in a production environment, you would not use max-instances=1
 echo "*** deploy ${SERVICE_NAME} service to ${CLOUDRUN_REGION} with service account ${SVCACCT_EMAIL} ***"
 gcloud run deploy ${SERVICE_NAME} \
-  --image=gcr.io/${GOOGLE_PROJECT_ID}/${SERVICE_NAME} \
+  --image=${CLOUDRUN_REGION}-docker.pkg.dev/${GOOGLE_PROJECT_ID}/${REPOSITORY_NAME}/${SERVICE_NAME} \
   --platform=managed \
   --max-instances=1 \
   --region=${CLOUDRUN_REGION} \
