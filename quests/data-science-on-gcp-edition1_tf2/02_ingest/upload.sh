59a77df01b387fd212f580b0cb08119ab25d46b2
Margubur Rahman <150442997+googlyrahman@users.noreply.github.com>
2025-10-21 13:48:23 +0530

    Manual changes to upload.sh

--- quests/data-science-on-gcp-edition1_tf2/02_ingest/upload.sh
+++ quests/data-science-on-gcp-edition1_tf2/02_ingest/upload.sh
@@ -2,4 +2,4 @@
 export BUCKET=${BUCKET:=cloud-training-demos-ml}
 echo "Uploading to bucket $BUCKET..."
 gcloud storage cp *.csv gs://$BUCKET/flights/raw
-#gcloud storage objects update --recursive --add-acl-grant=domain=google.com:READER gs://$BUCKET/flights/raw
+#gcloud storage objects update --recursive --add-acl-grant=entity=domain-google.com,role=READER gs://$BUCKET/flights/raw
